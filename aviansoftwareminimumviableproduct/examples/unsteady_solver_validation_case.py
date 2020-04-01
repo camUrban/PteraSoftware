1ec7e845ffa46fb4119cad7ab2f815dbf054b154
CamUrban <camerongurban@gmail.com>
2020-03-31 22:26:52 -0700

    Fixed panel movement method and made a method to produce of gif of the geometry movement

--- aviansoftwareminimumviableproduct/examples/unsteady_solver_validation_case.py
+++ aviansoftwareminimumviableproduct/examples/unsteady_solver_validation_case.py
@@ -1,82 +1,46 @@
-from aerosandbox_legacy_v0 import *
 from aviansoftwareminimumviableproduct import *
-import numpy
+import numpy as np
+import aerosandbox_legacy_v0 as asl
 
-
-unsteady_solver_validation_airplane = Airplane(
+unsteady_solver_validation_airplane = asl.Airplane(
     name="Unsteady Solver Validation Airplane",
     xyz_ref=[0.25, 0, 0],
     wings=[
-        Wing(
+        asl.Wing(
             name="Wing",
             xyz_le=[0, 0, 0],
             symmetric=True,
             xsecs=[
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 0, 0],
                     chord=1,
-                    airfoil=Airfoil(name="naca2412")
+                    airfoil=asl.Airfoil(name="naca2412")
                 ),
-                WingXSec(
-                    xyz_le=[0, 5, 0],
+                asl.WingXSec(
+                    xyz_le=[0, 4, 0],
                     chord=1,
-                    airfoil=Airfoil(name="naca2412")
-                )
-            ]
-        ),
-        Wing(
-            name="Horizontal Stabilizer",
-            xyz_le=[5, 0, 0.5],
-            symmetric=True,
-            xsecs=[
-                WingXSec(
-                    xyz_le=[0, 0, 0],
-                    chord=1,
-                    twist=-5,
-                    airfoil=Airfoil(name="naca0012")
-                ),
-                WingXSec(
-                    xyz_le=[0, 1, 0],
-                    chord=1,
-                    twist=-5,
-                    airfoil=Airfoil(name="naca0012")
-                )
-            ]
-
-        ),
-        Wing(
-            name="Vertical Stabilizer",
-            xyz_le=[5, 0, 0.5],
-            symmetric=False,
-            xsecs=[
-                WingXSec(
-                    xyz_le=[0, 0, 0],
-                    chord=1,
-                    airfoil=Airfoil(name="naca0012")
-                ),
-                WingXSec(
-                    xyz_le=[0, 0, 1.5],
-                    chord=1,
-                    airfoil=Airfoil(name="naca0012")
+                    airfoil=asl.Airfoil(name="naca2412")
                 )
             ]
         )
     ]
 )
 
-unsteady_solver_validation_operating_point = OperatingPoint(
+unsteady_solver_validation_operating_point = asl.OperatingPoint(
     velocity=10
 )
 
 unsteady_solver_validation_movement = Movement(
-    movement_period=1,
-    sweeping_amplitude=numpy.pi / 2
+    movement_period=0.5,
+    sweeping_amplitude=np.pi / 4
 )
 
-unsteady_solver_validation_problem = UnsteadyAerodynamicsProblem(
-    airplane=unsteady_solver_validation_airplane,
+unsteady_solver_validation_problem = UnsteadyVortexLatticeMethod1(
+    unsteady_solver_validation_airplane,
     operating_point=unsteady_solver_validation_operating_point,
     movement=unsteady_solver_validation_movement,
-    simulation_duration=10,
-    simulation_time_step=0.1
+    simulation_duration=1,
+    simulation_time_step=0.01
 )
+
+unsteady_solver_validation_problem.run()

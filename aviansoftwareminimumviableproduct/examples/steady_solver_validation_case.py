a516cd3d801835006632d422a0d121a91a151db9
CamUrban <camerongurban@gmail.com>
2020-03-31 22:26:52 -0700

    Fixed panel movement method and made a method to produce of gif of the geometry movement
    Former-commit-id: 2261ed9dc1e7a25d1fb72c07aca579d5fbfb8076 [formerly 26672a9ff00afccd0de0e2abe73c20e82e3546fd] [formerly 65cf1f7eecb95032b7ef70ca53438330c4bb2bae [formerly 1ec7e845ffa46fb4119cad7ab2f815dbf054b154]]
    Former-commit-id: 2f5d879248682c8c34e98197d600f4c0d05763a1 [formerly 45d596014a16c4394981001e9e923bff870bb1aa]
    Former-commit-id: 3b75376783195e8963779386d43da1736a679fe2

--- aviansoftwareminimumviableproduct/examples/steady_solver_validation_case.py
+++ aviansoftwareminimumviableproduct/examples/steady_solver_validation_case.py
@@ -1,69 +1,70 @@
-from aerosandbox_legacy_v0 import *
+import aerosandbox_legacy_v0 as asl
 
-steady_solver_validation_airplane = Airplane(
+
+steady_solver_validation_airplane = asl.Airplane(
     name="Steady Solver Validation Airplane",
     xyz_ref=[0.25, 0, 0],
     wings=[
-        Wing(
+        asl.Wing(
             name="Wing",
             xyz_le=[0, 0, 0],
             symmetric=True,
             xsecs=[
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 0, 0],
                     chord=1,
-                    airfoil=Airfoil(name="naca2412")
+                    airfoil=asl.Airfoil(name="naca2412")
                 ),
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 5, 0],
                     chord=1,
-                    airfoil=Airfoil(name="naca2412")
+                    airfoil=asl.Airfoil(name="naca2412")
                 )
             ]
         ),
-        Wing(
+        asl.Wing(
             name="Horizontal Stabilizer",
             xyz_le=[5, 0, 0.5],
             symmetric=True,
             xsecs=[
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 0, 0],
                     chord=1,
                     twist=-5,
-                    airfoil=Airfoil(name="naca0012")
+                    airfoil=asl.Airfoil(name="naca0012")
                 ),
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 1, 0],
                     chord=1,
                     twist=-5,
-                    airfoil=Airfoil(name="naca0012")
+                    airfoil=asl.Airfoil(name="naca0012")
                 )
             ]
 
         ),
-        Wing(
+        asl.Wing(
             name="Vertical Stabilizer",
             xyz_le=[5, 0, 0.5],
             symmetric=False,
             xsecs=[
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 0, 0],
                     chord=1,
-                    airfoil=Airfoil(name="naca0012")
+                    airfoil=asl.Airfoil(name="naca0012")
                 ),
-                WingXSec(
+                asl.WingXSec(
                     xyz_le=[0, 0, 1.5],
                     chord=1,
-                    airfoil=Airfoil(name="naca0012")
+                    airfoil=asl.Airfoil(name="naca0012")
                 )
             ]
         )
     ]
 )
 
-steady_solver_validation_problem = vlm3(
+steady_solver_validation_problem = asl.vlm3(
     airplane=steady_solver_validation_airplane,
-    op_point=OperatingPoint(
+    op_point=asl.OperatingPoint(
         velocity=10
     )
 )

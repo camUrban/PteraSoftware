a9831af181c0a772a8bd6c7b0505bf9f8abe186a
CamUrban <camerongurban@gmail.com>
2020-05-20 15:25:06 -0700

    Modified aerodynamics functions and debugged ring vortex method.
    Former-commit-id: bd215a57b77f87a2ed497306067f27e63c26c95a [formerly e177fd2ac40293bed25c0fe518010a06d3f48196] [formerly 0c5e98619a412e505d01880b2cfead44b181c144 [formerly 9a6b4428008ee880557acc835f9f892ab79da4fe]] [formerly f0b961ba7340c8e2e53d763b0ce00ecf18f2d7fa [formerly 4edfb3d61afc43edc64e61ae34d371a561323d35] [formerly 4b9d3571d3f5288a903ce046c48da8f9c9347c85 [formerly a36b4160a9e791aa706ee89c73b951cbf709d447]]]
    Former-commit-id: bdb44ad7379696751fe52a97b37ae45f6cda4e33 [formerly 4c2417710df93e6970c2c12192efbfb6c1977ab7] [formerly dabce7835d5896a73fe0484ea9b7485946b35efa [formerly 7e40c9641758f105b006c601b64f8fd749e3df50]]
    Former-commit-id: daeab7bf41c4c51ccfe0540fb5eacb5dd29952cd [formerly 3f6dbf25f4adb73281bfc39f379000fc1d91cb7d]
    Former-commit-id: e2f2c1faa16789f02fd4d7154bc770d8ef647364

--- aviansoftwareminimumviableproduct/geometry.py
+++ aviansoftwareminimumviableproduct/geometry.py
@@ -785,11 +785,7 @@ class Panel:
         calculate_area_and_normal: This method calculates the panel's area and the panel's normal unit vector.
         calculate_normalized_induced_velocity: This method calculates the velocity induced at a point by this panel's
                                                vortices, assuming a unit vortex strength.
-        calculate_normalized_induced_downwash: This method calculates the downwash induced at a point by this panel's
-                                               vortices, assuming a unit vortex strength.
         calculate_induced_velocity: This method calculates the velocity induced at a point by this panel's vortices with
-                                    their given vortex strengths.
-        calculate_induced_downwash: This method calculates the downwash induced at a point by this panel's vortices with
                                     their given vortex strengths.
         update_force_moment_and_pressure: This method updates the force, moment, and pressure on this panel based on the
                                           force on its vortices, its area, and its normal vector.
@@ -801,14 +797,14 @@ class Panel:
         This class is not meant to be subclassed.
     """
 
-    def __init__(self, front_left_vertex, front_right_vertex, back_left_vertex, back_right_vertex, is_leading_edge,
+    def __init__(self, front_right_vertex, front_left_vertex, back_left_vertex, back_right_vertex, is_leading_edge,
                  is_trailing_edge):
         """ This is the initialization method.
 
-        :param front_left_vertex: 1D ndarray with three elements
-            This is an array containing the x, y, and z coordinates of the panel's front left vertex.
         :param front_right_vertex: 1D ndarray with three elements
             This is an array containing the x, y, and z coordinates of the panel's front right vertex.
+        :param front_left_vertex: 1D ndarray with three elements
+            This is an array containing the x, y, and z coordinates of the panel's front left vertex.
         :param back_left_vertex: 1D ndarray with three elements
             This is an array containing the x, y, and z coordinates of the panel's back left vertex.
         :param back_right_vertex: 1D ndarray with three elements
@@ -820,8 +816,8 @@ class Panel:
         """
 
         # Initialize the attributes.
-        self.front_left_vertex = front_left_vertex
         self.front_right_vertex = front_right_vertex
+        self.front_left_vertex = front_left_vertex
         self.back_left_vertex = back_left_vertex
         self.back_right_vertex = back_right_vertex
         self.is_leading_edge = is_leading_edge
@@ -842,19 +838,20 @@ class Panel:
         self.calculate_area_and_normal()
 
         # Calculate the center of the panel.
-        self.center = asmvp.geometry.centroid_of_quadrilateral(front_left_vertex, front_right_vertex, back_left_vertex,
+        self.center = asmvp.geometry.centroid_of_quadrilateral(front_right_vertex, front_left_vertex, back_left_vertex,
                                                                back_right_vertex)
 
         # Calculate the front and back leg lengths, then use them to find and populate the average panel width.
-        front_leg_length = np.linalg.norm(front_right_vertex - front_left_vertex)
-        back_leg_length = np.linalg.norm(back_left_vertex - back_right_vertex)
+        front_leg_length = np.linalg.norm(front_left_vertex - front_right_vertex)
+        back_leg_length = np.linalg.norm(back_right_vertex - back_left_vertex)
         self.width = (front_leg_length + back_leg_length) / 2
 
         # Initialize two variables that are along the panel's left and right legs at the quarter chord. These points
         # are used for all types of solvers, so we will define them here.
-        self.front_left_vortex_vertex = self.front_left_vertex + 0.25 * (self.back_left_vertex - self.front_left_vertex)
         self.front_right_vortex_vertex = self.front_right_vertex + 0.25 * (self.back_right_vertex
                                                                            - self.front_right_vertex)
+        self.front_left_vortex_vertex = self.front_left_vertex + 0.25 * (self.back_left_vertex
+                                                                         - self.front_left_vertex)
 
         # Initialize a variable to hold the solver type. This will be filled after the solver initializes the vortices.
         self.solver_type = None
@@ -873,17 +870,17 @@ class Panel:
         """
 
         # Find the location of points three quarters of the way down the left and right legs of the panel.
-        left_three_quarter_chord_mark = self.front_left_vertex + 0.75 * (self.back_left_vertex
-                                                                         - self.front_left_vertex)
         right_three_quarter_chord_mark = self.front_right_vertex + 0.75 * (self.back_right_vertex
                                                                            - self.front_right_vertex)
+        left_three_quarter_chord_mark = self.front_left_vertex + 0.75 * (self.back_left_vertex
+                                                                         - self.front_left_vertex)
 
         # Find the vector between the points three quarters of the way down the left and right legs of the panel.
-        three_quarter_chord_vector = right_three_quarter_chord_mark - left_three_quarter_chord_mark
+        three_quarter_chord_vector = left_three_quarter_chord_mark - right_three_quarter_chord_mark
 
         # Find the collocation point, which is halfway between the points three quarters of the way down the left and
         # right legs of the panel. Then populate the class attribute.
-        self.collocation_point = left_three_quarter_chord_mark + 0.5 * three_quarter_chord_vector
+        self.collocation_point = right_three_quarter_chord_mark + 0.5 * three_quarter_chord_vector
 
     def calculate_area_and_normal(self):
         """This method calculates the panel's area and the panel's normal unit vector.
@@ -921,25 +918,6 @@ class Panel:
 
         return normalized_induced_velocity
 
-    def calculate_normalized_induced_downwash(self, point):
-        """This method calculates the downwash induced at a point by this panel's vortices, assuming a unit vortex
-        strength.
-
-        :param point: 1D ndarray
-            This is a vector containing the x, y, and z coordinates of the point to find the induced downwash at.
-        :return: 1D ndarray
-            This is a vector containing the x, y, and z components of the induced downwash.
-        """
-
-        normalized_induced_downwash = np.zeros(3)
-
-        if self.ring_vortex is not None:
-            normalized_induced_downwash += self.ring_vortex.calculate_normalized_induced_downwash(point=point)
-        if self.horseshoe_vortex is not None:
-            normalized_induced_downwash += self.horseshoe_vortex.calculate_normalized_induced_downwash(point=point)
-
-        return normalized_induced_downwash
-
     def calculate_induced_velocity(self, point):
         """This method calculates the velocity induced at a point by this panel's vortices with their given vortex
         strengths.
@@ -958,24 +936,6 @@ class Panel:
             induced_velocity += self.horseshoe_vortex.calculate_induced_velocity(point=point)
 
         return induced_velocity
-
-    def calculate_induced_downwash(self, point):
-        """This method calculates the downwash induced at a point by this panel's vortices with their given vortex
-        strengths.
-
-        :param point: 1D ndarray
-            This is a vector containing the x, y, and z coordinates of the point to find the induced downwash at.
-        :return: 1D ndarray
-            This is a vector containing the x, y, and z components of the induced downwash.
-        """
-        induced_downwash = np.zeros(3)
-
-        if self.ring_vortex is not None:
-            induced_downwash = self.ring_vortex.calculate_induced_downwash(point=point)
-        if self.horseshoe_vortex is not None:
-            induced_downwash += self.horseshoe_vortex.calculate_induced_downwash(point=point)
-
-        return induced_downwash
 
     def update_force_moment_and_pressure(self):
         """This method updates the force, moment, and pressure on this panel based on the force on its vortices,

076628ca059bf0688f4968c5ec476fb52956b87d
CamUrban <camerongurban@gmail.com>
2020-05-25 15:36:50 -0700

    Removed docstring issue.
    Former-commit-id: 73b00ed2d10bb7d756f4e40f3007d4f8c1b826a8 [formerly 3b57a5c6d1e53fc2ec0ab0cb502bd6c5b7261645] [formerly 971b8c844503bc1884583ab23d9eff9fe4a599cc [formerly 965f7b697aa59b7e7a8f42b8759be3e811afd218]]
    Former-commit-id: db072869d51736ff9f44ea2cf6905480d241e7d8 [formerly 87af78d6383cd96ef0a744c75396a9a1c60c7230]
    Former-commit-id: 4764d82656581ea333f132a6c95d86333021bc5f

--- tests/unsteady_ring_vortex_lattice_method_testing_case.py
+++ tests/unsteady_ring_vortex_lattice_method_testing_case.py
@@ -1,6 +1,4 @@
-"""This is a testing case for the unsteady ring vortex lattice method solver.
-
-"""
+"""This is a testing case for the unsteady ring vortex lattice method solver."""
 
 import aviansoftwareminimumviableproduct as asmvp
 

e822f218312ce63d2d07db7845c34ccfd2e6339c
CamUrban <camerongurban@gmail.com>
2020-06-11 15:28:05 -0700

    I formalized the testing suite.
    Former-commit-id: 3d03471bcff5b8c951a7a8769e0a0fc2bb21089f [formerly 9452f7a534005eeec05c495c96dcdc3acbcf879e] [formerly c39df1cc0a5289b9a5536b43880e7a93e3113c47 [formerly f17c4ad65b7c0c1e37373843fd1a353717c11da2]] [formerly d33b34c5aba2f1779fb460adfa9cbc30cbb94c2a [formerly 26bb077120e6285400fb67fecada9724204a1162] [formerly 7d3f16851034745007fcee2bae643c4f0951ab20 [formerly 9e866b804d558c3374d0ae3d509749721f9a6a53]]]
    Former-commit-id: 14031db18d604c626683ec7a25c3bf884feb9e22 [formerly bfd1a13c241a1a0dd47c974e53525d20d47afe85] [formerly 461fa86a3b16d617d4cdd69d8b531d9d1532217c [formerly 2cfa13ebbbeea2348dbf047820bc70f97d19910e]]
    Former-commit-id: 3c9b95ad4341068144570d719e6d9dd453cdc076 [formerly 465a92261b5d30963a8150837d1ae688b97d5756]
    Former-commit-id: 14dd0774c5abbe90ef1f12cf159fb42526c48e84

--- tests/unit/test_line_vortex.py
+++ tests/unit/test_line_vortex.py
@@ -8,6 +8,7 @@ import unittest
 import numpy as np
 
 import aviansoftwareminimumviableproduct as asmvp
+import tests.unit
 
 
 # ToDo: Properly document this class.
@@ -17,24 +18,151 @@ class TestLineVortex(unittest.TestCase):
     """
 
     # ToDo: Properly document this method.
+    def setUp(self):
+        """
+
+        :return: 
+        """
+
+        self.line_vortex_fixture = tests.unit.fixtures.vortex_fixtures.make_line_vortex_fixture()
+        self.origin_fixture = tests.unit.fixtures.vortex_fixtures.make_origin_fixture()
+        self.termination_fixture = tests.unit.fixtures.vortex_fixtures.make_termination_fixture()
+        self.strength_fixture = tests.unit.fixtures.vortex_fixtures.make_strength_fixture()
+
+    # ToDo: Properly document this method.
+    def tearDown(self):
+        """
+
+        :return: 
+        """
+
+        del self.line_vortex_fixture
+        del self.origin_fixture
+        del self.termination_fixture
+        del self.strength_fixture
+    
+    # ToDo: Properly document this method.
     def test_class(self):
         """
 
         :return:
         """
 
-        origin_fixture = np.zeros(3)
-        termination_fixture = np.ones(3)
-        strength_fixture = 10
-        line_vortex_fixture = asmvp.aerodynamics.LineVortex(
-            origin=origin_fixture,
-            termination=termination_fixture,
-            strength=strength_fixture
+        self.assertIsInstance(self.line_vortex_fixture, asmvp.aerodynamics.LineVortex)
+
+        self.assertTrue(np.allclose(self.line_vortex_fixture.origin, self.origin_fixture))
+        self.assertTrue(np.allclose(self.line_vortex_fixture.termination, self.termination_fixture))
+        self.assertEqual(self.line_vortex_fixture.strength, self.strength_fixture)
+
+        self.assertTrue(np.allclose(self.line_vortex_fixture.center, np.array([0.5, 0.5, 0.5])))
+        self.assertTrue(np.allclose(self.line_vortex_fixture.vector, np.array([1, 1, 1])))
+
+    # ToDo: Properly document this method.
+    def test_calculate_normalized_induced_velocity(self):
+        """
+
+        :return:
+        """
+
+        point_fixture = self.line_vortex_fixture.origin
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture), 0)
         )
 
-        self.assertIsInstance(line_vortex_fixture, asmvp.aerodynamics.LineVortex)
-        self.assertTrue(np.allclose(line_vortex_fixture.origin, origin_fixture))
-        self.assertTrue(np.allclose(line_vortex_fixture.termination, termination_fixture))
-        self.assertTrue(np.allclose(line_vortex_fixture.strength, strength_fixture))
-        self.assertTrue(np.allclose(line_vortex_fixture.center, np.array([0.5, 0.5, 0.5])))
-        self.assertTrue(np.allclose(line_vortex_fixture.vector, np.array([1, 1, 1])))
+        point_fixture = self.line_vortex_fixture.termination
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture), 0)
+        )
+
+        point_fixture = self.line_vortex_fixture.center
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture), 0)
+        )
+
+        point_fixture = self.line_vortex_fixture.center + self.line_vortex_fixture.vector
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture), 0)
+        )
+
+        point_fixture = self.line_vortex_fixture.center - self.line_vortex_fixture.vector
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture), 0)
+        )
+
+        point_fixture = self.line_vortex_fixture.center + np.array([1, 0, 0])
+
+        r_1_fixture = point_fixture - self.line_vortex_fixture.origin
+        r_2_fixture = point_fixture - self.line_vortex_fixture.termination
+        r_0_fixture = r_1_fixture - r_2_fixture
+        r_1_length_fixture = np.linalg.norm(r_1_fixture)
+        r_2_length_fixture = np.linalg.norm(r_2_fixture)
+
+        r_0_dot_r_1_fixture = np.dot(r_0_fixture, r_1_fixture)
+        r_0_dot_r_2_fixture = np.dot(r_0_fixture, r_2_fixture)
+
+        r_1_cross_r_2_fixture = np.cross(r_1_fixture, r_2_fixture)
+        r_1_cross_r_2_absolute_magnitude_fixture = (r_1_cross_r_2_fixture[0] ** 2
+                                                    + r_1_cross_r_2_fixture[1] ** 2
+                                                    + r_1_cross_r_2_fixture[2] ** 2)
+
+        normalized_induced_velocity = (
+                r_1_cross_r_2_fixture
+                / (4 * np.pi * r_1_cross_r_2_absolute_magnitude_fixture)
+                * (r_0_dot_r_1_fixture / r_1_length_fixture - r_0_dot_r_2_fixture / r_2_length_fixture)
+        )
+
+        self.assertTrue(
+            np.allclose(
+                self.line_vortex_fixture.calculate_normalized_induced_velocity(point=point_fixture),
+                normalized_induced_velocity
+            )
+        )
+
+    # ToDo: Properly document this method.
+    def test_calculate_induced_velocity(self):
+        """
+
+        :return:
+        """
+
+        point_fixture = self.line_vortex_fixture.origin
+        self.assertTrue(np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), 0))
+
+        point_fixture = self.line_vortex_fixture.termination
+        self.assertTrue(np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), 0))
+
+        point_fixture = self.line_vortex_fixture.center
+        self.assertTrue(np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), 0))
+
+        point_fixture = self.line_vortex_fixture.center + self.line_vortex_fixture.vector
+        self.assertTrue(np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), 0))
+
+        point_fixture = self.line_vortex_fixture.center - self.line_vortex_fixture.vector
+        self.assertTrue(np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), 0))
+
+        point_fixture = self.line_vortex_fixture.center + np.array([1, 0, 0])
+
+        r_1_fixture = self.line_vortex_fixture.origin - point_fixture
+        r_2_fixture = self.line_vortex_fixture.termination - point_fixture
+        r_0_fixture = r_1_fixture - r_2_fixture
+        r_1_length_fixture = np.linalg.norm(r_1_fixture)
+        r_2_length_fixture = np.linalg.norm(r_2_fixture)
+
+        r_0_dot_r_1_fixture = np.dot(r_0_fixture, r_1_fixture)
+        r_0_dot_r_2_fixture = np.dot(r_0_fixture, r_2_fixture)
+
+        r_1_cross_r_2_fixture = np.cross(r_1_fixture, r_2_fixture)
+        r_1_cross_r_2_absolute_magnitude_fixture = (r_1_cross_r_2_fixture[0] ** 2
+                                                    + r_1_cross_r_2_fixture[1] ** 2
+                                                    + r_1_cross_r_2_fixture[2] ** 2)
+
+        induced_velocity = (
+                r_1_cross_r_2_fixture
+                * self.line_vortex_fixture.strength
+                / (4 * np.pi * r_1_cross_r_2_absolute_magnitude_fixture)
+                * (r_0_dot_r_1_fixture / r_1_length_fixture - r_0_dot_r_2_fixture / r_2_length_fixture)
+        )
+
+        self.assertTrue(
+            np.allclose(self.line_vortex_fixture.calculate_induced_velocity(point=point_fixture), induced_velocity)
+        )

971350e42eb01b80c677fc15156dcde12645f7a0
CamUrban <camerongurban@gmail.com>
2020-06-17 16:03:59 -0700

    Debugging for Travis.
    Former-commit-id: 2c438be2c6a651dc09f94352fe7b80e70f14d072 [formerly 5154ec7c56741b8ca62839458ab2ad9495612f70] [formerly af8b5c699d5b7a9ab55f0fe2ba16a12ee713db20 [formerly ce84713243f881abd1d4663ed6e76bd391cf06b9]] [formerly a7e6113ceed8d9646e1c64e309c4edc80e35208a [formerly 0121551ecacfaa9cf1390f41ead4f8cc77dcbab9] [formerly ab18f54753a2df3f281bcb47901f7c64f9308847 [formerly e2b7b1f67a547aebc5f9220f6c698a0b6e890774]]]
    Former-commit-id: 0da821726144d0b51358feb1426e2dd7221f1eca [formerly b524eced50e7f0959081dea4572869c42a974f54] [formerly 9b6f00a624cc339adcaf35be8ecdb0dd68d23317 [formerly c040f898362ed6f1a5b7fabe9af99486c1cae445]]
    Former-commit-id: 89ad2a4df1a7cf0ee1a0c55271ecb765056d1171 [formerly 480533564194368a25788608743cd9fe6aa10bd3]
    Former-commit-id: face7bc8fec4b43d2a1807fe4de19b930b91d078

--- testing_in_development/quick_test.py
+++
@@ -1,7 +0,0 @@
-import tests.integration.fixtures.airplane_fixtures
-import aviansoftwareminimumviableproduct as asmvp
-
-
-airplane = tests.integration.fixtures.airplane_fixtures.make_steady_validation_airplane()
-
-asmvp.output.draw_geometry(airplane)

ee45c17a3f2c1c8d79f7e4e812f270f9243fa8a5
CamUrban <camerongurban@gmail.com>
2020-06-16 20:58:44 -0700

    Updates to unit tests.
    Former-commit-id: 65107e32456d5866bd217f1d55578d677789380a [formerly 7aa4d5d329e54bedf3170f08e007c82c2697e56b] [formerly 01d91fac6145a21042eaba077b833144e4747e4d [formerly 0d815107af0849156b5c54be1f62f17b5e31d88b]] [formerly 3920d2aa06b680b8521bfb82126d5f1c95ad8195 [formerly 5374cff9b6677162e414f293ed7ab675c2568028] [formerly 1d3180b7df9f5196b65a3e149bac0686fc22e151 [formerly a01be38e481265dab73b01b17884ab7c7a4ef546]]]
    Former-commit-id: 9b9b5177bdec76b00f0dec6695448e861469dfba [formerly 1037ec832626d56b03c451f1c77ad255e33def6b] [formerly 79ed9ae307fc5823572c116e262d298e4553691c [formerly 2b87a1e5ce97455e8967b32f75edc9e60387ca08]]
    Former-commit-id: 79ccd7afa23322b295c11721b7d14da92a1d5ee5 [formerly 7b0db8c6189da57728d1de2b2d0d3f1c6cff649f]
    Former-commit-id: c1e75f958ca129ea761434e63a4f89a03faa5551

--- tests/integration/fixtures/__init__.py
+++ tests/integration/fixtures/__init__.py
@@ -5,6 +5,7 @@
 """
 
 from tests.integration.fixtures import airplane_fixtures
+from tests.integration.fixtures import movement_fixtures
 from tests.integration.fixtures import operating_point_fixtures
 from tests.integration.fixtures import problem_fixtures
 from tests.integration.fixtures import solver_fixtures

0996b2ecf6a7edef1a07b7e70c41f81491c82c6d
CamUrban <camerongurban@gmail.com>
2020-06-24 13:02:01 -0700

    Finished documenting output.
    Former-commit-id: c30a8666a6a955f4815546cf96fa817055b8d03e [formerly 0989218ffe9d1412ef8ee8277c7669156a66ba64] [formerly 135b037c478666df2ec7de41dd92321cda5f75c5 [formerly 7f7522ec2e6e9333f8963ba75a1aac5d5c456140]] [formerly 53b5964ab0033e6c24ce2f941f8f58585d0ddb9b [formerly 7a0e45e9f6b4ebe0a700f440203e61ffbddeecba] [formerly 30a3b2aa85b6e935df67cfb5720e5e342f6b8e55 [formerly 1938cb2b9b6b02a42c85f39cfede21301cc45fab]]]
    Former-commit-id: 4623a4be4c779972237850a5a2cb98490cee2b5e [formerly 36abf3f9660549370c6a7ad730d7359c26a3f62e] [formerly 953468de89c54d60918c6fc3e85605baaab7b8b9 [formerly d717ea275a6eaf5b0aa16c5bb769a16d24d5e544]]
    Former-commit-id: 6af0d0fad9186d2d1b4a137894ff869d0e4536b7 [formerly b282390edf2a09bf528b9ca4d5e1c55493de2384]
    Former-commit-id: a2df6904f0901720fc8116a4cd6e762d8fdb01c5

--- tests/integration/test_output.py
+++ tests/integration/test_output.py
@@ -21,8 +21,8 @@ class TestUnsteadyRingVortexLatticeMethod(unittest.TestCase):
         :return:
         """
 
-        self.movement = (
-            tests.integration.fixtures.movement_fixtures.make_static_validation_movement()
+        self.unsteady_solver = (
+            tests.integration.fixtures.solver_fixtures.make_unsteady_ring_vortex_lattice_method_validation_solver_with_static_geometry()
         )
 
     # ToDo: Properly document this method.
@@ -32,7 +32,7 @@ class TestUnsteadyRingVortexLatticeMethod(unittest.TestCase):
         :return:
         """
 
-        del self.movement
+        del self.unsteady_solver
 
     # ToDo: Properly document this method.
     def test_plot_results_versus_time(self):
@@ -42,8 +42,37 @@ class TestUnsteadyRingVortexLatticeMethod(unittest.TestCase):
         """
         num_figs_before = plt.gcf().number
 
-        asmvp.output.plot_results_versus_time(movement=self.movement, verbose=False)
+        asmvp.output.plot_results_versus_time(
+            unsteady_solver=self.unsteady_solver, testing=True
+        )
 
         num_figs_after = plt.gcf().number
 
         self.assertEqual(num_figs_before + 4, num_figs_after)
+
+    # ToDo: Properly document this method.
+    def test_animate_does_not_throw(self):
+        """
+
+        :return:
+        """
+
+        asmvp.output.animate(
+            unsteady_solver=self.unsteady_solver,
+            show_delta_pressures=False,
+            show_wake_vortices=False,
+        )
+
+    # ToDo: Properly document this method.
+    def test_draw_does_not_throw(self):
+        """
+
+        :return:
+        """
+
+        asmvp.output.draw(
+            airplane=self.unsteady_solver.steady_problems[0].airplane,
+            show_delta_pressures=False,
+            show_wake_vortices=False,
+            show_streamlines=False,
+        )

677e6b3fe4e192bacc12eb1c5dc7e7a1e44ae6fb
CamUrban <camerongurban@gmail.com>
2020-06-24 15:52:43 -0700

    Continued documenting the movement module and finished documenting the vortex fixture module.
    Former-commit-id: 3a47a0740e13b0a3b0923861a2de5c0ece2ddc1a [formerly 35827852db5623388beb41529bfd9e653ba59577] [formerly ff1271dc7e99d9ff61d242ca9d1a2ff0a17a308f [formerly cb3458aa9160ae3e51c391b3f5be720b5339af57]]
    Former-commit-id: 0e921d95a82ede50ad858e01e1ddcc29cab0d772 [formerly ab510d230eb6259581ceb2917147d8be5137940c]
    Former-commit-id: ea76509edb0ec574e06d3229af218f0327293b91

--- tests/unit/fixtures/vortex_fixtures.py
+++ tests/unit/fixtures/vortex_fixtures.py
@@ -1,6 +1,24 @@
-# ToDo: Properly document this module.
-"""
+"""This module contains functions to create vortex objects for use in tests.
 
+This module contains the following classes:
+    None
+
+This module contains the following exceptions:
+    None
+
+This module contains the following functions:
+    make_origin_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_termination_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_strength_fixture: This method makes a fixture that holds a value.
+    make_line_vortex_fixture: This method makes a fixture that is a line vortex object.
+    make_infinite_leg_direction_fixture: This method makes a direction fixture.
+    make_infinite_leg_length_fixture: This method makes a fixture that holds a value.
+    make_horseshoe_vortex_fixture: This method makes a fixture that is a horseshoe vortex object.
+    make_front_left_vertex_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_front_right_vertex_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_back_left_vertex_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_back_right_vertex_fixture: This method makes a fixture that holds the coordinates of a position.
+    make_ring_vortex_fixture: This method makes a fixture that is a ring vortex object.
 """
 
 import numpy as np
@@ -8,93 +26,109 @@ import numpy as np
 import aviansoftwareminimumviableproduct as asmvp
 
 
-# ToDo: Properly document this method.
 def make_origin_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return termination: (,3) ndarray of floats
+        This holds the origin's coordinates.
     """
 
-    :return:
-    """
+    origin = np.zeros(3)
 
-    return np.zeros(3)
+    return origin
 
 
-# ToDo: Properly document this method.
 def make_termination_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return termination: (,3) ndarray of floats
+        This holds the termination's coordinates.
     """
 
-    :return:
-    """
+    termination = np.ones(3)
 
-    return np.ones(3)
+    return termination
 
 
-# ToDo: Properly document this method.
 def make_strength_fixture():
+    """ This method makes a fixture that holds a value.
+
+    :return strength: float
+        This is the strength of the vortex. Its units are meters squared per second.
     """
 
-    :return:
-    """
+    strength = 10.0
 
-    return 10
+    return strength
 
 
-# ToDo: Properly document this method.
 def make_line_vortex_fixture():
+    """This method makes a fixture that is a line vortex object.
+
+    :return line_vortex_fixture: LineVortex
+        This is the line vortex object.
     """
 
-    :return:
-    """
-
+    # Initialize the origin, termination, and strength.
     origin_fixture = make_origin_fixture()
     termination_fixture = make_termination_fixture()
     strength_fixture = make_strength_fixture()
 
+    # Create the line vortex object.
     line_vortex_fixture = asmvp.aerodynamics.LineVortex(
         origin=origin_fixture,
         termination=termination_fixture,
         strength=strength_fixture,
     )
 
+    # Delete the constructing fixtures.
     del origin_fixture
     del termination_fixture
     del strength_fixture
 
+    # Return the vortex fixture.
     return line_vortex_fixture
 
 
-# ToDo: Properly document this method.
 def make_infinite_leg_direction_fixture():
+    """ This method makes a direction fixture.
+
+    :return direction: (,3) ndarray of floats
+        This holds the direction's components.
     """
 
-    :return:
-    """
+    direction = np.array([1, 0, 0])
 
-    return np.array([1, 0, 0])
+    return direction
 
 
-# ToDo: Properly document this method.
 def make_infinite_leg_length_fixture():
+    """ This method makes a fixture that holds a value.
+
+    :return length: float
+        This is the length fixture.
     """
 
-    :return:
-    """
+    length = 20.0
 
-    return 20
+    return length
 
 
-# ToDo: Properly document this method.
 def make_horseshoe_vortex_fixture():
+    """ This method makes a fixture that is a horseshoe vortex object.
+
+    :return horseshoe_vortex_fixture: HorseshoeVortex
+        This is the horseshoe vortex object.
     """
 
-    :return:
-    """
-
+    # Initialize the constructing fixtures.
     origin_fixture = make_origin_fixture()
     termination_fixture = make_termination_fixture()
     strength_fixture = make_strength_fixture()
     infinite_leg_direction_fixture = make_infinite_leg_direction_fixture()
     infinite_leg_length_fixture = make_infinite_leg_length_fixture()
 
+    # Create the horseshoe vortex object.
     horseshoe_vortex_fixture = asmvp.aerodynamics.HorseshoeVortex(
         finite_leg_origin=origin_fixture,
         finite_leg_termination=termination_fixture,
@@ -103,68 +137,80 @@ def make_horseshoe_vortex_fixture():
         infinite_leg_length=infinite_leg_length_fixture,
     )
 
+    # Delete the constructing fixtures.
     del origin_fixture
     del termination_fixture
     del strength_fixture
     del infinite_leg_direction_fixture
     del infinite_leg_length_fixture
 
+    # Return the horseshoe vortex fixture.
     return horseshoe_vortex_fixture
 
 
-# ToDo: Properly document this method.
 def make_front_left_vertex_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return front_left_vertex: (,3) ndarray of floats
+        This holds the fixture's coordinates.
     """
 
-    :return:
-    """
+    front_left_vertex = np.zeros(3)
 
-    return np.zeros(3)
+    return front_left_vertex
 
 
-# ToDo: Properly document this method.
 def make_front_right_vertex_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return front_right_vertex: (,3) ndarray of floats
+        This holds the fixture's coordinates.
     """
 
-    :return:
-    """
+    front_right_vertex = np.ones(3)
 
-    return np.ones(3)
+    return front_right_vertex
 
 
-# ToDo: Properly document this method.
 def make_back_left_vertex_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return back_left_vertex: (,3) ndarray of floats
+        This holds the fixture's coordinates.
     """
 
-    :return:
-    """
+    back_left_vertex = np.array([1, 1, 0])
 
-    return np.array([1, 1, 0])
+    return back_left_vertex
 
 
-# ToDo: Properly document this method.
 def make_back_right_vertex_fixture():
+    """ This method makes a fixture that holds the coordinates of a position.
+
+    :return back_right_vertex: (,3) ndarray of floats
+        This holds the fixture's coordinates.
     """
 
-    :return:
-    """
+    back_right_vertex = np.array([1, 1, 0])
 
-    return np.array([1, 0, 0])
+    return back_right_vertex
 
 
-# ToDo: Properly document this method.
 def make_ring_vortex_fixture():
+    """ This method makes a fixture that is a ring vortex object.
+
+    :return ring_vortex_fixture: RingVortex
+        This is the ring vortex object.
     """
 
-    :return:
-    """
-
+    # Initialize the constructing fixtures.
     front_left_vertex_fixture = make_front_left_vertex_fixture()
     front_right_vertex_fixture = make_front_right_vertex_fixture()
     back_left_vertex_fixture = make_back_left_vertex_fixture()
     back_right_vertex_fixture = make_back_right_vertex_fixture()
     strength_fixture = make_strength_fixture()
 
+    # Create the ring vortex object.
     ring_vortex_fixture = asmvp.aerodynamics.RingVortex(
         front_left_vertex=front_left_vertex_fixture,
         front_right_vertex=front_right_vertex_fixture,
@@ -173,10 +219,12 @@ def make_ring_vortex_fixture():
         strength=strength_fixture,
     )
 
+    # Delete the constructing fixtures.
     del front_left_vertex_fixture
     del front_right_vertex_fixture
     del back_left_vertex_fixture
     del back_right_vertex_fixture
     del strength_fixture
 
+    # Return the ring vortex fixture.
     return ring_vortex_fixture

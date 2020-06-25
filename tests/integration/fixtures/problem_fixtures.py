0483c4a0e0cdc82cc8c765eeab9bca2c27f498cc
CamUrban <camerongurban@gmail.com>
2020-06-25 14:36:24 -0700

    Optimized imports and kept documenting.
    Former-commit-id: 0b0ee4dfdfca3a959578b18be203a376e2013863 [formerly caa19f4ef03ae6d264ee14e6ac79335c78b0fb28] [formerly 47af589a2cec9bd9b3c54e613a60917f901d2114 [formerly ab0fa8b99a355e491a3bafd05df7d15a28e512bd]]
    Former-commit-id: f0e0b36d4d901289cb83ff23dc073d9700a47207 [formerly c29e337433a7bba771040ec4264808c05888eea7]
    Former-commit-id: 37d7faff379fb58b5028503eaa8cd53b12bbd3a9

--- tests/integration/fixtures/problem_fixtures.py
+++ tests/integration/fixtures/problem_fixtures.py
@@ -1,19 +1,33 @@
-# ToDo: Properly document this module.
 """ This module creates problem objects to be used as fixtures.
 
+This module contains the following classes:
+    None
+
+This module contains the following exceptions:
+    None
+
+This module contains the following functions:
+    make_steady_validation_problem: This function creates a steady problem object to be used as a fixture.
+    make_steady_multiple_wing_validation_problem: This function creates a steady problem object with multi-wing geometry
+                                                  to be used as a fixture.
+    make_unsteady_validation_problem_with_static_geometry: This function creates an unsteady problem object with static
+                                                           geometry to be used as a fixture.
+    make_unsteady_validation_problem_with_variable_geometry: This function creates an unsteady problem object with
+                                                             variable geometry to be used as a fixture.
 """
 
 import aviansoftwareminimumviableproduct as asmvp
 import tests as tests
 
 
-# ToDo: Properly document this method.
 def make_steady_validation_problem():
+    """ This function creates a steady problem object to be used as a fixture.
+
+    :return steady_validation_problem: SteadyProblem
+        This is the problem fixture.
     """
 
-    :return:
-    """
-
+    # Create the constructing fixtures.
     steady_validation_airplane = (
         tests.integration.fixtures.airplane_fixtures.make_steady_validation_airplane()
     )
@@ -21,24 +35,28 @@ def make_steady_validation_problem():
         tests.integration.fixtures.operating_point_fixtures.make_validation_operating_point()
     )
 
+    # Create the problem fixture.
     steady_validation_problem = asmvp.problems.SteadyProblem(
         airplane=steady_validation_airplane,
         operating_point=steady_validation_operating_point,
     )
 
+    # Delete the constructing fixtures.
     del steady_validation_airplane
     del steady_validation_operating_point
 
+    # Return the problem fixture.
     return steady_validation_problem
 
 
-# ToDo: Properly document this method.
 def make_steady_multiple_wing_validation_problem():
+    """ This function creates a steady problem object with multi-wing geometry to be used as a fixture.
+
+    :return steady_validation_problem: SteadyProblem
+        This is the problem fixture.
     """
 
-    :return:
-    """
-
+    # Create the constructing fixtures.
     steady_validation_airplane = (
         tests.integration.fixtures.airplane_fixtures.make_multiple_wing_steady_validation_airplane()
     )
@@ -46,52 +64,63 @@ def make_steady_multiple_wing_validation_problem():
         tests.integration.fixtures.operating_point_fixtures.make_validation_operating_point()
     )
 
+    # Create the problem fixture.
     steady_validation_problem = asmvp.problems.SteadyProblem(
         airplane=steady_validation_airplane,
         operating_point=steady_validation_operating_point,
     )
 
+    # Delete the constructing fixtures.
     del steady_validation_airplane
     del steady_validation_operating_point
 
+    # Return the problem fixture.
     return steady_validation_problem
 
 
-# ToDo: Properly document this method.
 def make_unsteady_validation_problem_with_static_geometry():
+    """ This function creates an unsteady problem object with static geometry to be used as a fixture.
+
+    :return unsteady_validation_problem: UnsteadyProblem
+        This is the problem fixture.
     """
 
-    :return:
-    """
-
+    # Create the constructing fixture.
     unsteady_validation_movement = (
         tests.integration.fixtures.movement_fixtures.make_static_validation_movement()
     )
 
+    # Create the problem fixture.
     unsteady_validation_problem = asmvp.problems.UnsteadyProblem(
         movement=unsteady_validation_movement
     )
 
+    # Delete the constructing fixture.
     del unsteady_validation_movement
 
+    # Return the problem fixture.
     return unsteady_validation_problem
 
 
-# ToDo: Properly document this method.
 def make_unsteady_validation_problem_with_variable_geometry():
+    """ This function creates an unsteady problem object with variable geometry to be used as a fixture.
+
+    :return unsteady_validation_problem: UnsteadyProblem
+        This is the problem fixture.
     """
 
-    :return:
-    """
-
+    # Create the constructing fixture.
     unsteady_validation_movement = (
         tests.integration.fixtures.movement_fixtures.make_variable_validation_movement()
     )
 
+    # Create the problem fixture.
     unsteady_validation_problem = asmvp.problems.UnsteadyProblem(
         movement=unsteady_validation_movement
     )
 
+    # Delete the constructing fixture.
     del unsteady_validation_movement
 
+    # Return the problem fixture.
     return unsteady_validation_problem

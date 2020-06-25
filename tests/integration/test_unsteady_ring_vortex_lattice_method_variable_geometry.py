d13e5d236fbb760640ee28d4ff6bd65e21995816
CamUrban <camerongurban@gmail.com>
2020-06-25 15:21:36 -0700

    Attempted fix of cyclic import error.
    Former-commit-id: 4892cf4bc5523f2c1663e08eacf7c2baf334b322 [formerly 7af1a266bea63b500f2c243ae04b33dfa773b287] [formerly 9e7ae26dc41ace2797784481760d375cb5246cb8 [formerly 0b6ed5e66bce6b42f3abf0710d7518f97966926c]] [formerly 0792dabcfa53219c6d69754e4eeee95ab103ec7d [formerly 6ed742337b6ac786374a41a831742a8c63e85aaf] [formerly 7167d9354645554cb40975d979378d965d4a6b3e [formerly d88ce538e6cece850003b9f49dd7dec50ae72762]]]
    Former-commit-id: 4069b9c75a201f68b3efb113de38207f538ca595 [formerly 9b2227fd6141288bc3cab6a0dde772cc274ce294] [formerly c26683d500328e9f426c1e6e79f5e633c6315128 [formerly 28d4aee7861414e449943014257521fe772ec4d6]]
    Former-commit-id: 210cb1bb4b3a74b1e51fe5569c2dc62a611c21fb [formerly 21bc289a0061d81d98d2d9a10a3defee06c02388]
    Former-commit-id: 0afd3e50378e315cb9071d635251a21372b0172f

--- tests/integration/test_unsteady_ring_vortex_lattice_method_variable_geometry.py
+++ tests/integration/test_unsteady_ring_vortex_lattice_method_variable_geometry.py
@@ -13,7 +13,7 @@ This module contains the following functions:
 
 import unittest
 
-import tests.integration
+import tests.integration.fixtures
 
 
 class TestUnsteadyRingVortexLatticeMethodVariableGeometry(unittest.TestCase):

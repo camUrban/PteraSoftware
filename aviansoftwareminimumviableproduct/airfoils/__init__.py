a686e53901bb465682922e7a9f8912db5bd35769
CamUrban <camerongurban@gmail.com>
2020-06-25 12:51:37 -0700

    Continued documenting.
    Former-commit-id: 1c50e9beef15344b76106fb7e99b053cae22f1c6 [formerly c33ffe6fa2b2bb777dedcec449feea46dff32807] [formerly 2c03539d6a52be0ff2a6d406faf2c3981212e375 [formerly 30ff4074fe0421aee98ea7241411e53349ded95e]] [formerly 4deaca17d8b83b4c534151d8a0be05efe5293b2e [formerly 635b04d7114c9c8fdc58d0151cb6ddc6cdc22b0d] [formerly 81214ba4ede1858c83f073a5d6385cc0ab22053c [formerly cc17d9f9ce396c06dbd25003ff5ba06b9c5cdd06]]]
    Former-commit-id: 7b0f0b03fcc263e2b1d2cd0e418c03e6850dea45 [formerly fa917d95674b6c5ceeb87a2be8dff48b77484756] [formerly 2036aa1b154513e884e266c6d7b4cd62eedd7cab [formerly e5bd0976bcf026abc0f727a3b2197d7a5f4da4d7]]
    Former-commit-id: 492d927a80cf9b7f8d4d276ad9662944b08fd5d8 [formerly 2c0a1fb30763a640e035e33cba29490860539228]
    Former-commit-id: 8d3a48b36fc0d50e0a739ccee18ed0a0d09b36e4

---
+++ aviansoftwareminimumviableproduct/airfoils/__init__.py
@@ -0,0 +1,1 @@
+""" This module doesn't import anything. It is only here to make the airfoils directory a package."""
--- tests/integration/fixtures/movement_fixtures.py
+++ tests/integration/fixtures/movement_fixtures.py
@@ -1,19 +1,30 @@
-# ToDo: Properly document this module.
 """ This module creates movement objects to be used as fixtures.
 
+This module contains the following classes:
+    None
+
+This module contains the following exceptions:
+    None
+
+This module contains the following functions:
+    make_static_validation_movement: This function creates a movement object with static geometry to be used as a
+                                     fixture.
+    make_variable_validation_movement: This function creates a movement object with variable geometry to be used as a
+                                       fixture.
 """
 
 import aviansoftwareminimumviableproduct as asmvp
 import tests as tests
 
 
-# ToDo: Properly document this method.
 def make_static_validation_movement():
+    """ This function creates a movement with static geometry to be used as a fixture.
+
+    :return unsteady_validation_movement: Movement
+        This is a movement object with static geometry to be used as a fixture.
     """
 
-    :return:
-    """
-
+    # Construct an airplane object and an operating point object.
     unsteady_validation_airplane = (
         tests.integration.fixtures.airplane_fixtures.make_asymmetric_unsteady_validation_airplane()
     )
@@ -21,18 +32,21 @@ def make_static_validation_movement():
         tests.integration.fixtures.operating_point_fixtures.make_validation_operating_point()
     )
 
+    # Create a wing cross section movement object associated with this airplane's root wing cross section.
     unsteady_validation_root_wing_cross_section_movement = asmvp.movement.WingCrossSectionMovement(
         base_wing_cross_section=unsteady_validation_airplane.wings[
             0
         ].wing_cross_sections[0]
     )
 
+    # Create a wing cross section movement object associated with this airplane's tip wing cross section.
     unsteady_validation_tip_wing_cross_section_movement = asmvp.movement.WingCrossSectionMovement(
         base_wing_cross_section=unsteady_validation_airplane.wings[
             0
         ].wing_cross_sections[1],
     )
 
+    # Create a wing movement object associated with this airplane's wing.
     unsteady_validation_wing_movement = asmvp.movement.WingMovement(
         base_wing=unsteady_validation_airplane.wings[0],
         wing_cross_sections_movements=[
@@ -41,23 +55,29 @@ def make_static_validation_movement():
         ],
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_root_wing_cross_section_movement
     del unsteady_validation_tip_wing_cross_section_movement
 
+    # Create an airplane movement object associated with this airplane.
     unsteady_validation_airplane_movement = asmvp.movement.AirplaneMovement(
         base_airplane=unsteady_validation_airplane,
         wing_movements=[unsteady_validation_wing_movement],
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_airplane
     del unsteady_validation_wing_movement
 
+    # Create an operating point movement object associated with this operating point.
     unsteady_validation_operating_point_movement = asmvp.movement.OperatingPointMovement(
         base_operating_point=unsteady_validation_operating_point
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_operating_point
 
+    # Create a movement object associated with this airplane and operating point.
     unsteady_validation_movement = asmvp.movement.Movement(
         airplane_movement=unsteady_validation_airplane_movement,
         operating_point_movement=unsteady_validation_operating_point_movement,
@@ -65,19 +85,22 @@ def make_static_validation_movement():
         delta_time=1 / 6 / 10,
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_airplane_movement
     del unsteady_validation_operating_point_movement
 
+    # Return the movement fixture.
     return unsteady_validation_movement
 
 
-# ToDo: Properly document this method.
 def make_variable_validation_movement():
+    """ This function creates a movement with variable geometry to be used as a fixture.
+
+    :return unsteady_validation_movement: Movement
+        This is a movement object with variable geometry to be used as a fixture.
     """
 
-    :return:
-    """
-
+    # Construct an airplane object and an operating point object.
     unsteady_validation_airplane = (
         tests.integration.fixtures.airplane_fixtures.make_symmetric_unsteady_validation_airplane()
     )
@@ -85,12 +108,14 @@ def make_variable_validation_movement():
         tests.integration.fixtures.operating_point_fixtures.make_validation_operating_point()
     )
 
+    # Create a wing cross section movement object associated with this airplane's root wing cross section.
     unsteady_validation_root_wing_cross_section_movement = asmvp.movement.WingCrossSectionMovement(
         base_wing_cross_section=unsteady_validation_airplane.wings[
             0
         ].wing_cross_sections[0],
     )
 
+    # Create a wing cross section movement object associated with this airplane's tip wing cross section.
     unsteady_validation_tip_wing_cross_section_movement = asmvp.movement.WingCrossSectionMovement(
         base_wing_cross_section=unsteady_validation_airplane.wings[
             0
@@ -100,6 +125,7 @@ def make_variable_validation_movement():
         z_le_spacing="uniform",
     )
 
+    # Create a wing movement object associated with this airplane's wing.
     unsteady_validation_wing_movement = asmvp.movement.WingMovement(
         base_wing=unsteady_validation_airplane.wings[0],
         wing_cross_sections_movements=[
@@ -108,25 +134,31 @@ def make_variable_validation_movement():
         ],
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_root_wing_cross_section_movement
     del unsteady_validation_tip_wing_cross_section_movement
 
+    # Create an airplane movement object associated with this airplane.
     unsteady_validation_airplane_movement = asmvp.movement.AirplaneMovement(
         base_airplane=unsteady_validation_airplane,
         wing_movements=[unsteady_validation_wing_movement],
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_airplane
     del unsteady_validation_wing_movement
 
+    # Create an operating point movement object associated with this operating point.
     unsteady_validation_operating_point_movement = asmvp.movement.OperatingPointMovement(
         base_operating_point=unsteady_validation_operating_point,
         velocity_amplitude=1.0,
         velocity_period=0.2,
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_operating_point
 
+    # Create a movement object associated with this airplane and operating point.
     unsteady_validation_movement = asmvp.movement.Movement(
         airplane_movement=unsteady_validation_airplane_movement,
         operating_point_movement=unsteady_validation_operating_point_movement,
@@ -134,7 +166,9 @@ def make_variable_validation_movement():
         delta_time=1 / 6 / 10,
     )
 
+    # Delete the now extraneous constructing fixtures.
     del unsteady_validation_airplane_movement
     del unsteady_validation_operating_point_movement
 
+    # Return the movement fixture.
     return unsteady_validation_movement

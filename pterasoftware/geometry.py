Fixed bug with calculating rotation matrix for wing cross section twisting.
Former-commit-id: c40a66f373ae60324166057b96ff688013bc0798 [formerly c3c386900e89505f36be662c0f89fd9855b76b0e] [formerly f983361a8778fa3a5e240414195365d3d0836720 [formerly 6669ddbdb88d530b6207e80cf7ffd1f902392ac1]]
Former-commit-id: 81f8981f2b45753bebf87663f1ac9956e110bbf4 [formerly 7ced53526691031d7df06d1c9a360fc4fc9b65f6]
Former-commit-id: 6a60017700ca38a5472fb76a913c7d1b695584c6

pterasoftware/geometry.py
@@ -1173,30 +1173,33 @@ def angle_axis_rotation_matrix(angle, axis, axis_already_normalized=False):
     if not axis_already_normalized:
         axis = axis / np.linalg.norm(axis)
 
+    # Define constants to use when calculating the rotation matrix.
     sin_theta = np.sin(angle)
     cos_theta = np.cos(angle)
-    cpm = np.array(
-        [[0, -axis[2], axis[1]], [axis[2], 0, -axis[0]], [-axis[1], axis[0], 0]]
+    u_x = axis[0]
+    u_y = axis[1]
+    u_z = axis[2]
+
+    # Calculate the rotation matrix.
+    rotation_matrix = np.array(
+        [
+            [
+                cos_theta + u_x ** 2 * (1 - cos_theta),
+                u_x * u_y * (1 - cos_theta) - u_z * sin_theta,
+                u_x * u_z * (1 - cos_theta) + u_y * sin_theta,
+            ],
+            [
+                u_y * u_x * (1 - cos_theta) + u_z * sin_theta,
+                cos_theta + u_y ** 2 * (1 - cos_theta),
+                u_y * u_z * (1 - cos_theta) - u_x * sin_theta,
+            ],
+            [
+                u_z * u_x * (1 - cos_theta) - u_y * sin_theta,
+                u_z * u_y * (1 - cos_theta) + u_x * sin_theta,
+                cos_theta + u_z ** 2 * (1 - cos_theta),
+            ],
+        ]
     )
-
-    # Find the cross product matrix of the rotation axis vector.
-    outer_axis = axis @ np.transpose(axis)
-
-    # Make sure angle is a ndarray.
-    angle = np.array(angle)
-
-    # Check if the angle is a scalar.
-    if len(angle.shape) == 0:
-        rotation_matrix = (
-            cos_theta * np.eye(3) + sin_theta * cpm + (1 - cos_theta) * outer_axis
-        )
-    else:
-        # Otherwise, the angle is assumed to be a 1D ndarray.
-        rotation_matrix = (
-            cos_theta * np.expand_dims(np.eye(3), 2)
-            + sin_theta * np.expand_dims(cpm, 2)
-            + (1 - cos_theta) * np.expand_dims(outer_axis, 2)
-        )
 
     # Return the rotation matrix.
     return rotation_matrix
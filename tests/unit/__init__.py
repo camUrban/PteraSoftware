b8d371a45ed9388151907ea56100986f5c8bc173
camUrban <camerongurban@gmail.com>
2021-07-09 13:33:51 -0400

    I added in the ability to specify the air's kinematic viscosity in the operating point class. I also cleaned up some documentation.

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,4 +1,21 @@
-# ToDo: Document this file.
+"""This package contains the unit tests.
+
+This package contains the following subpackages:
+    fixtures: This package contains the fixtures for unit testing.
+
+This package contains the following directories:
+    None
+
+This package contains the following modules:
+    __init__.py: This module is this package's initialization script.
+
+    test_horseshoe_vortex.py: This module contains a class to test horseshoe vortex
+    objects.
+
+    test_line_vortex.py: This module contains a class to test line vortex objects.
+
+    test_ring_vortex.py: This module contains a class to test ring vortex objects.
+"""
 import tests.unit.test_horseshoe_vortex
 import tests.unit.test_line_vortex
 import tests.unit.test_ring_vortex

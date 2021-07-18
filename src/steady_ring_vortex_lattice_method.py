e5d1df3ab78a960894eeaeab82b993b3fdd363dd
camUrban <camerongurban@gmail.com>
2021-07-18 18:13:45 -0400

    I consolidated the three solvers' streamline methods into one.

--- src/steady_ring_vortex_lattice_method.py
+++ src/steady_ring_vortex_lattice_method.py
@@ -14,6 +14,7 @@ This module contains the following functions:
 import numpy as np
 
 from . import aerodynamics
+from . import functions
 
 
 class SteadyRingVortexLatticeMethodSolver:
@@ -166,7 +167,7 @@ class SteadyRingVortexLatticeMethodSolver:
         # Solve for the location of the streamlines coming off the back of the wings.
         if verbose:
             print("\nCalculating streamlines.")
-        self.calculate_streamlines()
+        functions.calculate_streamlines(self)
 
         # Print out the total forces.
         if verbose:
@@ -875,44 +876,3 @@ class SteadyRingVortexLatticeMethodSolver:
                 yawing_moment_coefficient,
             ]
         )
-
-    def calculate_streamlines(self, num_steps=10, delta_time=0.1):
-        """Calculates the location of the streamlines coming off the back of the wings.
-
-        :param num_steps: int, optional
-            This is the integer number of points along each streamline (not including
-            the initial points). It can be increased for higher fidelity visuals. The
-            default value is 10.
-        :param delta_time: float, optional
-            This is the time in seconds between each time current_step It can be
-            decreased for higher fidelity visuals or to make the streamlines shorter.
-            It's default value is 0.1 seconds.
-        :return: None
-        """
-        # Initialize a array to hold this problem's matrix of streamline points.
-        self.streamline_points = np.expand_dims(self.seed_points, axis=0)
-
-        # Iterate through the streamline steps.
-        for step in range(num_steps):
-            # Get the last row of streamline points.
-            last_row_streamline_points = self.streamline_points[-1, :, :]
-
-            # Add the freestream velocity to the induced velocity to get the total
-            # velocity at each of the last row of streamline points.
-            total_velocities = self.calculate_solution_velocity(
-                points=last_row_streamline_points
-            )
-
-            # Interpolate the positions on a new row of streamline points.
-            new_row_streamline_points = (
-                last_row_streamline_points + total_velocities * delta_time
-            )
-
-            # Stack the new row of streamline points to the bottom of the matrix of
-            # streamline points.
-            self.streamline_points = np.vstack(
-                (
-                    self.streamline_points,
-                    np.expand_dims(new_row_streamline_points, axis=0),
-                )
-            )

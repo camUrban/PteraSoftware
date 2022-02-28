I fixed a significant error where the pressures on the wings were calculated incorrectly due to a sign error in the flapping velocities.

pterasoftware/output.py
@@ -31,9 +31,9 @@ This module contains the following functions:
     get_wake_ring_vortex_surfaces: This function returns the PolyData object for the
     surface of wake ring vortices at a given time step.
 
-    get_scalars: This function gets the delta pressure values from a problem's
-    airplane objects, and puts them into a 1D array to be used as scalars for display
-    by other output methods.
+    get_scalars: This function gets the coefficient values from a problem's airplane
+    objects, and puts them into a 1D array to be used as scalars for display by other
+    output methods.
 """
 import os
 
@@ -100,7 +100,7 @@ marker_spacing = 1.0 / num_markers
 
 def draw(
     solver,
-    show_delta_pressures=False,
+    scalar_type=None,
     show_streamlines=False,
     show_wake_vortices=False,
 ):
@@ -114,10 +114,12 @@ def draw(
     :param solver: SteadyHorseshoeVortexLatticeMethodSolver or
     SteadyRingVortexLatticeMethodSolver or UnsteadyRingVortexLatticeMethodSolver
         This is the solver object whose geometry and attributes are to be plotted.
-    :param show_delta_pressures: bool, optional
-        Set this variable to true to show the change in pressure across the panels.
-        See the src.panel.update_pressure() method for more details on the pressure
-        value. The default value is false.
+    :param scalar_type: str or None, optional
+        This variable determines which values will be used to color the wing panels,
+        if any. The default value is None, which gives colors all wing panels
+        uniformly. Acceptable alternatives are "induced drag", "side force",
+        and "lift", which respectively use each panel's induced drag, side force,
+        or lift coefficient.
     :param show_streamlines: bool, optional
         Set this variable to true to show the streamlines emanating from the back of
         the wings. The default value is False.
@@ -148,18 +150,26 @@ def draw(
                 smooth_shading=True,
                 color=wake_vortex_color,
             )
-
     else:
         airplanes = solver.airplanes
+
+    # Check if the user wants to show scalars on the wing panels.
+    show_scalars = False
+    if (
+        scalar_type == "induced drag"
+        or scalar_type == "side force"
+        or scalar_type == "lift"
+    ):
+        show_scalars = True
 
     # Get the panel surfaces.
     panel_surfaces = get_panel_surfaces(airplanes)
 
-    # Check if the user wants to plot pressures.
-    if show_delta_pressures:
+    # Check if the user wants to plot any scalars.
+    if show_scalars:
 
         # Get the scalars
-        scalars = get_scalars(airplanes)
+        scalars = get_scalars(airplanes, scalar_type)
 
         # Choose the color map and set its limits based on if the min and max scalars
         # have the same sign (sequential color map) or if they have different signs
@@ -177,16 +187,16 @@ def draw(
             c_min = -color_map_num_sig * np.std(scalars)
             c_max = color_map_num_sig * np.std(scalars)
 
-        # Add the panel surfaces to the plotter with the pressure scalars.
+        # Add the panel surfaces to the plotter with the scalars.
         scalar_bar_args = dict(
-            title="Normal Pressure (Pa)",
+            title=scalar_type.title() + " Coefficient",
             title_font_size=bar_title_font_size,
             label_font_size=bar_label_font_size,
             width=bar_width,
             position_x=bar_position_x,
             position_y=bar_position_y,
             n_labels=bar_n_labels,
-            fmt="%.1f",
+            fmt="%.2f",
         )
         plotter.add_mesh(
             panel_surfaces,
@@ -198,13 +208,13 @@ def draw(
             scalar_bar_args=scalar_bar_args,
         )
         plotter.add_text(
-            text="Max: " + str(round(max(scalars), 1)) + " Pa",
+            text="Max: " + str(round(max(scalars), 2)),
             position=text_max_position,
             font_size=text_font_size,
             viewport=True,
         )
         plotter.add_text(
-            text="Min: " + str(round(min(scalars), 1)) + " Pa",
+            text="Min: " + str(round(min(scalars), 2)),
             position=text_min_position,
             font_size=text_font_size,
             viewport=True,
@@ -249,12 +259,13 @@ def draw(
 
     # Set the plotter background color and show the plotter.
     plotter.set_background(color=plotter_background_color)
+    plotter.add_camera_orientation_widget()
     plotter.show(cpos=(-1, -1, 1), full_screen=False)
 
 
 def animate(
     unsteady_solver,
-    show_delta_pressures=False,
+    scalar_type=None,
     show_wake_vortices=False,
     keep_file=True,
 ):
@@ -262,10 +273,12 @@ def animate(
 
     :param unsteady_solver: UnsteadyRingVortexLatticeMethodSolver
         This is the solver object whose geometry is to be animated.
-    :param show_delta_pressures: bool, optional
-        Set this variable to true to show the change in pressure across the panels.
-        See the src.panel.update_pressure() method for more details on the pressure
-        value. The default value is false.
+    :param scalar_type: str or None, optional
+        This variable determines which values will be used to color the wing panels,
+        if any. The default value is None, which gives colors all wing panels
+        uniformly. Acceptable alternatives are "induced drag", "side force",
+        and "lift", which respectively use each panel's induced drag, side force,
+        or lift coefficient.
     :param show_wake_vortices: bool, optional
         Set this variable to true to show the airplane object's wake ring vortices.
         The default value is false.
@@ -293,16 +306,25 @@ def animate(
     c_max = 0
     color_map = None
 
+    # Check if the user wants to show scalars on the wing panels.
+    show_scalars = False
+    if (
+        scalar_type == "induced drag"
+        or scalar_type == "side force"
+        or scalar_type == "lift"
+    ):
+        show_scalars = True
+
     # Initialize an empty array to hold all of the problem's scalars.
     all_scalars = np.empty(0, dtype=int)
 
-    # Check if the user wants to show pressures.
-    if show_delta_pressures:
+    # Check if the user wants to show scalars on the wing panels.
+    if show_scalars:
 
         # Now iterate through each time step and gather all of the scalars for its
         # list of airplanes. These values will be used to configure the color map.
         for airplanes in step_airplanes:
-            scalars_to_add = get_scalars(airplanes)
+            scalars_to_add = get_scalars(airplanes, scalar_type)
             all_scalars = np.hstack((all_scalars, scalars_to_add))
 
         # Choose the color map and set its limits based on if the min and max scalars
@@ -326,22 +348,21 @@ def animate(
     # Initialize the panel surfaces and add the meshes to the plotter.
     panel_surfaces = get_panel_surfaces(step_airplanes[0])
 
-    # Check if the user wants to plot pressures. If so, add the panel surfaces to the
-    # plotter with the pressure scalars. Otherwise, add the panel surfaces without
-    # the pressure scalars.
-    if show_delta_pressures and first_results_step == 0:
-        scalars = get_scalars(step_airplanes[0])
+    # Check if the user wants to show any scalars. If so, add the panel surfaces to
+    # the plotter with these scalars.
+    if show_scalars and first_results_step == 0:
+        scalars = get_scalars(step_airplanes[0], scalar_type)
 
-        # Add the panel surfaces to the plotter with the pressure scalars.
+        # Add the panel surfaces to the plotter with the scalars.
         scalar_bar_args = dict(
-            title="Normal Pressure (Pa)",
+            title=scalar_type.title() + " Coefficient",
             title_font_size=bar_title_font_size,
             label_font_size=bar_label_font_size,
             width=bar_width,
             position_x=bar_position_x,
             position_y=bar_position_y,
             n_labels=bar_n_labels,
-            fmt="%.1f",
+            fmt="%.2f",
         )
         plotter.add_mesh(
             panel_surfaces,
@@ -353,19 +374,19 @@ def animate(
             scalar_bar_args=scalar_bar_args,
         )
         plotter.add_text(
-            text="Max: " + str(round(max(all_scalars), 1)) + " Pa",
+            text="Max: " + str(round(max(all_scalars), 2)),
             position=text_max_position,
             font_size=text_font_size,
             viewport=True,
         )
         plotter.add_text(
-            text="Min: " + str(round(min(all_scalars), 1)) + " Pa",
+            text="Min: " + str(round(min(all_scalars), 2)),
             position=text_min_position,
             font_size=text_font_size,
             viewport=True,
         )
 
-        # Update the scalars is the user wants to show the pressures.
+        # Update the scalars is the user wants to show.
         plotter.update_scalars(scalars)
     else:
         plotter.add_mesh(
@@ -385,7 +406,8 @@ def animate(
     )
 
     # Set up the camera and close the window.
-    plotter.show(cpos=(-1, -1, 1), full_screen=False, auto_close=False)
+    plotter.add_camera_orientation_widget()
+    plotter.show(cpos=(1, 1, 1), full_screen=False, auto_close=False)
 
     # Open a gif.
     plotter.open_gif("animation.gif")
@@ -415,22 +437,23 @@ def animate(
                 color=wake_vortex_color,
             )
 
-        # Check if the user wants to plot pressures and this step is equal to or
+        # Check if the user wants to plot scalars and this step is equal to or
         # greater than the first step with calculated results. If so, add the panel
-        # surfaces to the plotter with the pressure scalars. Otherwise, add the panel
-        # surfaces without the pressure scalars.
-        if show_delta_pressures and first_results_step <= current_step:
-            scalars = get_scalars(airplanes)
-            # Add the panel surfaces to the plotter with the pressure scalars.
+        # surfaces to the plotter with the scalars.
+        if show_scalars and first_results_step <= current_step:
+
+            scalars = get_scalars(airplanes, scalar_type)
+
+            # Add the panel surfaces to the plotter with the scalars.
             scalar_bar_args = dict(
-                title="Normal Pressure (Pa)",
+                title=scalar_type.title() + " Coefficient",
                 title_font_size=bar_title_font_size,
                 label_font_size=bar_label_font_size,
                 width=bar_width,
                 position_x=bar_position_x,
                 position_y=bar_position_y,
                 n_labels=bar_n_labels,
-                fmt="%.1f",
+                fmt="%.2f",
             )
             plotter.add_mesh(
                 panel_surfaces,
@@ -442,13 +465,13 @@ def animate(
                 scalar_bar_args=scalar_bar_args,
             )
             plotter.add_text(
-                text="Max: " + str(round(max(all_scalars), 1)) + " Pa",
+                text="Max: " + str(round(max(all_scalars), 2)),
                 position=text_max_position,
                 font_size=text_font_size,
                 viewport=True,
             )
             plotter.add_text(
-                text="Min: " + str(round(min(all_scalars), 1)) + " Pa",
+                text="Min: " + str(round(min(all_scalars), 2)),
                 position=text_min_position,
                 font_size=text_font_size,
                 viewport=True,
@@ -481,7 +504,7 @@ def animate(
 
 def animate_frames(
     unsteady_solver,
-    show_delta_pressures=False,
+    scalar_type=None,
     show_wake_vortices=False,
 ):
     """This function creates an animation of the solver's geometries but it saves
@@ -494,10 +517,12 @@ def animate_frames(
 
     :param unsteady_solver: UnsteadyRingVortexLatticeMethodSolver
         This is the solver object whose geometry is to be animated.
-    :param show_delta_pressures: bool, optional
-        Set this variable to true to show the change in pressure across the panels.
-        See the src.panel.update_pressure() method for more details on the pressure
-        value. The default value is false.
+    :param scalar_type: str or None, optional
+        This variable determines which values will be used to color the wing panels,
+        if any. The default value is None, which gives colors all wing panels
+        uniformly. Acceptable alternatives are "induced drag", "side force",
+        and "lift", which respectively use each panel's induced drag, side force,
+        or lift coefficient.
     :param show_wake_vortices: bool, optional
         Set this variable to true to show the airplane object's wake ring vortices.
         The default value is false.
@@ -505,6 +530,15 @@ def animate_frames(
     """
 
     first_results_step = unsteady_solver.first_results_step
+
+    # Check if the user wants to show scalars on the wing panels.
+    show_scalars = False
+    if (
+        scalar_type == "induced drag"
+        or scalar_type == "side force"
+        or scalar_type == "lift"
+    ):
+        show_scalars = True
 
     # Get this solver's problems' airplanes. This will become a list of lists,
     # with the first index being the time step and the second index identifying each
@@ -526,13 +560,13 @@ def animate_frames(
     # Initialize an empty array to hold all of the problem's scalars.
     all_scalars = np.empty(0, dtype=int)
 
-    # Check if the user wants to show pressures.
-    if show_delta_pressures:
+    # Configure the color map if the user wants to show scalars.
+    if show_scalars:
 
         # Now iterate through each time step and gather all of the scalars for its
         # list of airplanes. These values will be used to configure the color map.
         for airplanes in step_airplanes:
-            scalars_to_add = get_scalars(airplanes)
+            scalars_to_add = get_scalars(airplanes, scalar_type)
             all_scalars = np.hstack((all_scalars, scalars_to_add))
 
         # Choose the color map and set its limits based on if the min and max scalars
@@ -556,13 +590,12 @@ def animate_frames(
     # Initialize the panel surfaces and add the meshes to the plotter.
     panel_surfaces = get_panel_surfaces(step_airplanes[0])
 
-    # Check if the user wants to plot pressures. If so, add the panel surfaces to the
-    # plotter with the pressure scalars. Otherwise, add the panel surfaces without
-    # the pressure scalars.
-    if show_delta_pressures and first_results_step == 0:
-        scalars = get_scalars(step_airplanes[0])
+    # Check if the user wants to plot scalars. If so, add the panel surfaces to the
+    # plotter with the scalars.
+    if show_scalars and first_results_step == 0:
+        scalars = get_scalars(step_airplanes[0], scalar_type)
 
-        # Add the panel surfaces to the plotter with the pressure scalars.
+        # Add the panel surfaces to the plotter with the scalars.
         plotter.add_mesh(
             panel_surfaces,
             show_edges=True,
@@ -573,7 +606,7 @@ def animate_frames(
             show_scalar_bar=False,
         )
 
-        # Update the scalars is the user wants to show the pressures.
+        # Update the scalars is the user wants to show.
         plotter.update_scalars(scalars)
     else:
         plotter.add_mesh(
@@ -590,6 +623,7 @@ def animate_frames(
     )
 
     # Set up the camera and close the window.
+    plotter.add_camera_orientation_widget()
     plotter.show(cpos=(-1, -1, 1), full_screen=False, auto_close=False)
 
     plotter.screenshot(filename="0", transparent_background=True)
@@ -619,14 +653,13 @@ def animate_frames(
                 color=wake_vortex_color,
             )
 
-        # Check if the user wants to plot pressures and this step is equal to or
+        # Check if the user wants to plot scalars and this step is equal to or
         # greater than the first step with calculated results. If so, add the panel
-        # surfaces to the plotter with the pressure scalars. Otherwise, add the panel
-        # surfaces without the pressure scalars.
-        if show_delta_pressures and first_results_step <= current_step:
-            scalars = get_scalars(airplanes)
+        # surfaces to the plotter with the scalars.
+        if show_scalars and first_results_step <= current_step:
+            scalars = get_scalars(airplanes, scalar_type)
 
-            # Add the panel surfaces to the plotter with the pressure scalars.
+            # Add the panel surfaces to the plotter with the scalars.
             plotter.add_mesh(
                 panel_surfaces,
                 show_edges=True,
@@ -1375,15 +1408,20 @@ def get_wake_ring_vortex_surfaces(solver, step):
 
 def get_scalars(
     airplanes,
+    scalar_type,
 ):
-    """This function gets the delta pressure values from a problem's airplane
-    objects, and puts them into a 1D array to be used as scalars for display by other
-    output methods.
+    """This function gets the coefficient values from a problem's airplane objects,
+    and puts them into a 1D array to be used as scalars for display by other output
+    methods.
 
     :param airplanes: list of Airplane objects
         This is the list of airplane objects with the scalars we are collecting.
+    :param scalar_type: str
+        This variable determines which scalar values will be returned. Acceptable
+        inputs are "induced drag", "side force", and "lift", which respectively
+        return each panel's induced drag, side force, or lift coefficient.
     :return scalars: 1D array of ints
-        This is the 1D array of integers for each panel's delta pressure values.
+        This is the 1D array of integers for each panel's coefficient value.
     """
 
     # Initialize an empty array to hold the scalars.
@@ -1397,10 +1435,13 @@ def get_scalars(
             panels = np.ravel(wing.panels)
             for panel in panels:
 
-                # Stack this panel's scalars. The scalar is the pressure on the
-                # panel. See the src.panel.update_pressure() method for more details
-                # on the pressure value.
-                scalars = np.hstack((scalars, panel.delta_pressure))
+                # Stack this panel's scalars.
+                if scalar_type == "induced drag":
+                    scalars = np.hstack((scalars, panel.induced_drag_coefficient))
+                if scalar_type == "side force":
+                    scalars = np.hstack((scalars, panel.side_force_coefficient))
+                if scalar_type == "lift":
+                    scalars = np.hstack((scalars, panel.lift_coefficient))
 
     # Return the resulting 1D array of scalars.
     return scalars

pterasoftware/unsteady_ring_vortex_lattice_method.py
@@ -61,21 +61,21 @@ class UnsteadyRingVortexLatticeMethodSolver:
         populate_next_airplanes_wake_vortices: This method populates the locations of
         the next airplanes' wake vortices.
 
-        calculate_current_flapping_velocities_at_collocation_points: This method gets
-        the velocity due to flapping at all of the current airplanes' collocation
-        points.
+        calculate_current_flapping_velocities_at_collocation_points: This method
+        finds the apparent flow velocity due to flapping at the centers of the
+        current airplanes' collocation points.
 
-        calculate_current_flapping_velocities_at_right_leg_centers: This method gets
-        the velocity due to flapping at the centers of the current airplanes' bound
-        ring vortices' right legs.
+        calculate_current_flapping_velocities_at_right_leg_centers: This method finds
+        the apparent flow velocity due to flapping at the centers of the current
+        airplanes' bound ring vortices' right legs.
 
-        calculate_current_flapping_velocities_at_front_leg_centers: This method gets
-        the velocity due to flapping at the centers of the current airplanes' bound
-        ring vortices' front legs.
+        calculate_current_flapping_velocities_at_front_leg_centers: This method finds
+        the apparent flow velocity due to flapping at the centers of the current
+        airplanes' bound ring vortices' front legs.
 
-        calculate_current_flapping_velocities_at_left_leg_centers: This method gets
-        the velocity due to flapping at the centers of the current airplanes' bound
-        ring vortices' left legs.
+        calculate_current_flapping_velocities_at_left_leg_centers: This method finds
+        the apparent flow velocity due to flapping at the centers of the current
+        airplanes' bound ring vortices' left legs.
 
     This class contains the following class attributes:
         None
@@ -1057,117 +1057,6 @@ class UnsteadyRingVortexLatticeMethodSolver:
             near_field_forces_geometry_axes=near_field_forces_geometry_axes,
             near_field_moments_geometry_axes=near_field_moments_geometry_axes,
         )
-        # # Initialize a variable to hold the global panel position.
-        # global_panel_position = 0
-        #
-        # # Iterate through this solver's panels.
-        # for panel in self.panels:
-        #
-        #     # Update the force and moment on this panel.
-        #     panel.near_field_force_geometry_axes = near_field_forces_geometry_axes[
-        #         global_panel_position, :
-        #     ]
-        #     panel.near_field_moment_geometry_axes = near_field_moments_geometry_axes[
-        #         global_panel_position, :
-        #     ]
-        #
-        #     # Update the pressure on this panel.
-        #     panel.update_pressure()
-        #
-        #     # Increment the global panel position.
-        #     global_panel_position += 1
-        #
-        # # Sum up the near field forces and moments on every panel to find the total
-        # # force and moment on the geometry.
-        # total_near_field_force_geometry_axes = np.sum(
-        #     near_field_forces_geometry_axes, axis=0
-        # )
-        # total_near_field_moment_geometry_axes = np.sum(
-        #     near_field_moments_geometry_axes, axis=0
-        # )
-        #
-        # # ToDo: Fix the next block of code by separating forces by airplane.
-        # # Find the total near field force in wind axes from the rotation matrix and
-        # # the total near field force in geometry axes.
-        # # self.current_airplanes.total_near_field_force_wind_axes = 0
-        # # (
-        # #     np.transpose(
-        # #         self.current_operating_point.calculate_rotation_matrix_wind_to_geometry()
-        # #     )
-        # #     @ total_near_field_force_geometry_axes
-        # # )
-        #
-        # # Find the total near field moment in wind axes from the rotation matrix and
-        # # the total near field moment in geometry axes.
-        # # self.current_airplanes.total_near_field_moment_wind_axes = 0
-        # # (
-        # #     np.transpose(
-        # #         self.current_operating_point.calculate_rotation_matrix_wind_to_geometry()
-        # #     )
-        # #     @ total_near_field_moment_geometry_axes
-        # # )
-        #
-        # # Calculate the current_airplane's induced drag coefficient
-        # induced_drag_coefficient = 0
-        # # (
-        # #     -self.current_airplanes.total_near_field_force_wind_axes[0]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # # )
-        #
-        # # Calculate the current_airplane's side force coefficient.
-        # side_force_coefficient = 0
-        # # (
-        # #     self.current_airplanes.total_near_field_force_wind_axes[1]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # # )
-        #
-        # # Calculate the current_airplane's lift coefficient.
-        # lift_coefficient = 0
-        # # (
-        # #     -self.current_airplanes.total_near_field_force_wind_axes[2]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # # )
-        #
-        # # Calculate the current_airplane's rolling moment coefficient.
-        # rolling_moment_coefficient = 0
-        # # (
-        # #     self.current_airplanes.total_near_field_moment_wind_axes[0]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # #     / self.current_airplanes.b_ref
-        # # )
-        #
-        # # Calculate the current_airplane's pitching moment coefficient.
-        # pitching_moment_coefficient = 0
-        # # (
-        # #     self.current_airplanes.total_near_field_moment_wind_axes[1]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # #     / self.current_airplanes.c_ref
-        # # )
-        #
-        # # Calculate the current_airplane's yawing moment coefficient.
-        # yawing_moment_coefficient = 0
-        # # (
-        # #     self.current_airplanes.total_near_field_moment_wind_axes[2]
-        # #     / self.current_operating_point.calculate_dynamic_pressure()
-        # #     / self.current_airplanes.s_ref
-        # #     / self.current_airplanes.b_ref
-        # # )
-        #
-        # # self.current_airplanes.total_near_field_force_coefficients_wind_axes = np.array(
-        # #     [induced_drag_coefficient, side_force_coefficient, lift_coefficient]
-        # # )
-        # # self.current_airplanes.total_near_field_moment_coefficients_wind_axes = np.array(
-        # #     [
-        # #         rolling_moment_coefficient,
-        # #         pitching_moment_coefficient,
-        # #         yawing_moment_coefficient,
-        # #     ]
-        # # )
 
     def populate_next_airplanes_wake(self, prescribed_wake=True):
         """This method updates the next time step's airplanes' wakes.
@@ -1578,8 +1467,11 @@ class UnsteadyRingVortexLatticeMethodSolver:
                                     )
 
     def calculate_current_flapping_velocities_at_collocation_points(self):
-        """This method gets the velocity due to flapping at all of the current
-        airplanes' collocation points.
+        """This method finds the apparent flow velocity due to flapping at the
+        centers of the current airplanes' collocation points.
+
+        Note: The apparent flow velocity due to flapping is opposite the direction of
+        the wing's motion.
 
         :return: size (M x 3) array of floats, where M is the current airplanes'
         number of panels
@@ -1601,11 +1493,14 @@ class UnsteadyRingVortexLatticeMethodSolver:
         last_collocations = self.last_panel_collocation_points
 
         # Calculate and return the flapping velocities.
-        return (these_collocations - last_collocations) / self.delta_time
+        return -(these_collocations - last_collocations) / self.delta_time
 
     def calculate_current_flapping_velocities_at_right_leg_centers(self):
-        """This method gets the velocity due to flapping at the centers of the
-        current airplanes' bound ring vortices' right legs.
+        """This method finds the apparent flow velocity due to flapping at the
+        centers of the current airplanes' bound ring vortices' right legs.
+
+        Note: The apparent flow velocity due to flapping is opposite the direction of
+        the wing's motion.
 
         :return: size (M x 3) array of floats, where M is the current airplanes'
         number of panels
@@ -1627,11 +1522,14 @@ class UnsteadyRingVortexLatticeMethodSolver:
         last_right_leg_centers = self.last_panel_right_vortex_centers
 
         # Calculate and return the flapping velocities.
-        return (these_right_leg_centers - last_right_leg_centers) / self.delta_time
+        return -(these_right_leg_centers - last_right_leg_centers) / self.delta_time
 
     def calculate_current_flapping_velocities_at_front_leg_centers(self):
-        """This method gets the velocity due to flapping at the centers of the
-        current airplanes' bound ring vortices' front legs.
+        """This method finds the apparent flow velocity due to flapping at the
+        centers of the current airplanes' bound ring vortices' front legs.
+
+        Note: The apparent flow velocity due to flapping is opposite the direction of
+        the wing's motion.
 
         :return: size (M x 3) array of floats, where M is the current airplanes'
         number of panels
@@ -1653,11 +1551,14 @@ class UnsteadyRingVortexLatticeMethodSolver:
         last_front_leg_centers = self.last_panel_front_vortex_centers
 
         # Calculate and return the flapping velocities.
-        return (these_front_leg_centers - last_front_leg_centers) / self.delta_time
+        return -(these_front_leg_centers - last_front_leg_centers) / self.delta_time
 
     def calculate_current_flapping_velocities_at_left_leg_centers(self):
-        """This method gets the velocity due to flapping at the centers of the
-        current airplanes' bound ring vortices' left legs.
+        """This method finds the apparent flow velocity due to flapping at the
+        centers of the current airplanes' bound ring vortices' left legs.
+
+        Note: The apparent flow velocity due to flapping is opposite the direction of
+        the wing's motion.
 
         :return: size (M x 3) array of floats, where M is the current airplanes'
         number of panels
@@ -1679,4 +1580,4 @@ class UnsteadyRingVortexLatticeMethodSolver:
         last_left_leg_centers = self.last_panel_left_vortex_centers
 
         # Calculate and return the flapping velocities.
-        return (these_left_leg_centers - last_left_leg_centers) / self.delta_time
+        return -(these_left_leg_centers - last_left_leg_centers) / self.delta_time

validation/validation_study.py
@@ -299,7 +299,7 @@ def normalized_validation_geometry_sweep_function_rad(time):
     b_4 = -3.60e-6
 
     # Calculate and return the flap angle(s).
-    flap_angle = (
+    flap_angle = -(
         a_0
         + a_1 * np.cos(1 * time)
         + b_1 * np.sin(1 * time)
@@ -360,6 +360,12 @@ validation_operating_point = ps.operating_point.OperatingPoint(
 # Define an operating point movement that contains the operating point.
 validation_operating_point_movement = ps.movement.OperatingPointMovement(
     base_operating_point=validation_operating_point,
+)
+
+# Calculate the wind-to-geometry rotation matrix, which will later be used to convert
+# the experimental pressure-based-lift measurements to wind axes.
+wind_to_geometry_rotation_matrix = (
+    validation_operating_point.calculate_rotation_matrix_wind_to_geometry()
 )
 
 # Delete the extraneous pointer.
@@ -440,10 +446,9 @@ validation_solver.run(prescribed_wake=True)
 # Call the software’s animate function on the solver. This produces a GIF. The GIF is
 # saved in the same directory as this script. Press "q," after orienting the view,
 # to begin the animation.
-# ps.output.animate(  # Set the unsteady solver to the one we just ran.
-#     unsteady_solver=validation_solver,  # Show the pressures in the animation.
-#     show_delta_pressures=True,
-#     # Set this value to False to hide the wake vortices in the animation.
+# ps.output.animate(
+#     unsteady_solver=validation_solver,
+#     scalar_type="lift",
 #     show_wake_vortices=True,
 # )
 
@@ -609,11 +614,8 @@ exp_green_leading_lift_forces = exp_green_leading_normal_forces * np.cos(
 # Calculate the net experimental lift. This is the sum of all the lift on each of the
 # experimental panels multiplied by two (because the experimental panels only cover
 # one of the symmetric wing halves). Note: this list of lift forces is with respect
-# to the body axes. I will later compare it to the simulated lift in wind axes. This
-# does not matter because the operating point is at zero angle of attack. If the
-# angle of attack is changed, the experimental lift forces must be rotated to the
-# wind frame before comparison with the simulated lift forces.
-exp_net_lift_forces = 2 * (
+# to the geometry axes.
+exp_lifts_geometry_axes = 2 * (
     exp_blue_trailing_lift_forces
     + exp_blue_middle_lift_forces
     + exp_blue_leading_lift_forces
@@ -624,6 +626,17 @@ exp_net_lift_forces = 2 * (
     + exp_green_middle_lift_forces
     + exp_green_leading_lift_forces
 )
+
+# Initialize an array to hold the frame shifted lift forces.
+exp_lifts_wind_axes = np.zeros(exp_lifts_geometry_axes.size)
+
+# To compare the experimental lift to the simulated lift, use the rotation matrix to
+# shift the experimental lift to the wind frame.
+for lift_id, lift_geometry_axes in enumerate(exp_lifts_geometry_axes):
+    exp_force_geometry_axes = np.array([0, 0, lift_geometry_axes])
+    exp_force_wind_axes = wind_to_geometry_rotation_matrix @ exp_force_geometry_axes
+    exp_lift_wind_axes = exp_force_wind_axes[2]
+    exp_lifts_wind_axes[lift_id] = exp_lift_wind_axes
 
 # Get this solver’s problem’s airplanes.
 airplanes = []
@@ -668,7 +681,7 @@ lift_axes.plot(
 # Plot the experimental lift forces.
 lift_axes.plot(
     normalized_times,
-    exp_net_lift_forces,
+    exp_lifts_wind_axes,
     label="Experimental",
     color="#E62128",
     linestyle="dashed",
@@ -699,12 +712,12 @@ del step
 # and simulated lifts time histories are discretized so that they they are with
 # respect to the same time scale.
 lift_absolute_errors = np.abs(
-    final_flap_sim_net_lift_forces_wind_axes - exp_net_lift_forces
+    final_flap_sim_net_lift_forces_wind_axes - exp_lifts_wind_axes
 )
 lift_mean_absolute_error = np.mean(lift_absolute_errors)
 
 sim_lift_rms = math.sqrt(np.mean(final_flap_sim_net_lift_forces_wind_axes ** 2))
-exp_lift_rms = math.sqrt(np.mean(exp_net_lift_forces ** 2))
+exp_lift_rms = math.sqrt(np.mean(exp_lifts_wind_axes ** 2))
 lift_rmsape = 100 * abs((sim_lift_rms - exp_lift_rms) / exp_lift_rms)
 print("\nLift RMS Absolute Percent Error: " + str(np.round(lift_rmsape, 2)) + "%")
 print("Simulated Lift RMS: " + str(np.round(sim_lift_rms, 4)) + " N")
@@ -716,7 +729,11 @@ print(
 )
 
 # Calculate the experimental root mean square (RMS) lift.
-exp_rms_lift = np.sqrt(np.mean(np.power(exp_net_lift_forces, 2)))
+exp_rms_lift = np.sqrt(np.mean(np.power(exp_lifts_wind_axes, 2)))
 
 # Print the experimental RMS lift.
 print("Experimental RMS Lift: " + str(np.round(exp_rms_lift, 4)) + " N")
+
+ps.output.animate(
+    unsteady_solver=validation_solver, show_wake_vortices=True, scalar_type="lift"
+)
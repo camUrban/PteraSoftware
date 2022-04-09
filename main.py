bc173287bdce2a0229ce2fcb2a8e40c2d1829131
Zach10a <zachtait84@outlook.com>
2022-04-09 15:34:57 +0100

    Add splash

--- main.py
+++ main.py
@@ -17,15 +17,32 @@ print("QtGUI imported")
 from PySide2.QtWidgets import QDialog, QMainWindow, QApplication, QListWidgetItem, QSplashScreen, QMessageBox, QSlider, \
     QLabel, QAbstractItemView, QWidget, QColorDialog, QMenu
 
-from pterasoftware.ui_resources import mainWindow
+from pterasoftware.ui_resources.mainWindow import Ui_MainWindowDesign
 
-class MainWindow(QMainWindow, mainWindow):
-    if 0==0:
-        print("Duh")
+class MainWindow(QMainWindow, Ui_MainWindowDesign):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
 
+    app = QApplication(sys.argv)
+    pixmap = QPixmap('docs/logo.png')
+    splash = QSplashScreen(pixmap)
+    splash.setWindowFlags(Qt.WindowStaysOnTopHint)
+    splash.setEnabled(False)
+    splash.setMask(pixmap.mask())
+    splash.show()
+    time.sleep(0.1)  # This seems to fix the splash mask displaying but not the actual image
+    app.processEvents()
+
+    window = MainWindow()
+    window.show()
+    window.raise_()
+    window.activateWindow()
+    splash.finish(window)
+    sys.exit(app.exec_())
 
     print("Yup, it works")
 

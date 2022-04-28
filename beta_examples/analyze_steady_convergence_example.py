2a1e151ceb4bbe6673a9bd4830617d3efda75368
Cameron Urban <camerongurban@gmail.com>
2022-04-28 19:13:10 -0400

    I added a draft of a convergence analysis function.

---
+++ beta_examples/analyze_steady_convergence_example.py
@@ -0,0 +1,60 @@
+import logging
+
+import pterasoftware as ps
+
+# Configure a logger for this example.
+example_logger = logging.getLogger("example")
+example_logger.setLevel(logging.DEBUG)
+
+# Create an airplane object. Read through the solver examples for more details on
+# creating this object.
+default_airplane = ps.geometry.Airplane(
+    weight=250,
+    wings=[
+        ps.geometry.Wing(
+            symmetric=True,
+            chordwise_spacing="uniform",
+            wing_cross_sections=[
+                ps.geometry.WingCrossSection(
+                    airfoil=ps.geometry.Airfoil(
+                        name="naca2412",
+                    ),
+                    spanwise_spacing="cosine",
+                ),
+                ps.geometry.WingCrossSection(
+                    x_le=0.0,
+                    y_le=5.0,
+                    z_le=0.0,
+                    chord=1.0,
+                    airfoil=ps.geometry.Airfoil(
+                        name="naca2412",
+                    ),
+                ),
+                ps.geometry.WingCrossSection(
+                    x_le=0.25,
+                    y_le=10.0,
+                    z_le=0.0,
+                    chord=1.0,
+                    airfoil=ps.geometry.Airfoil(
+                        name="naca2412",
+                    ),
+                ),
+            ],
+        ),
+    ],
+)
+
+# Create an operating point object for this example's problem. Be sure to specify an
+# external thrust because this aircraft is not flapping, and will therefore generate
+# no thrust of its own. This external thrust could be due to a propeller or other
+# type of engine.
+default_operating_point = ps.operating_point.OperatingPoint()
+
+# Construct this example's problem object.
+default_problem = ps.problems.SteadyProblem(
+    airplanes=[default_airplane], operating_point=default_operating_point
+)
+
+trim_conditions = ps.convergence.analyze_steady_convergence(
+    base_problem=default_problem
+)

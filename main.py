9eb5aed6b81efd3b5635bad6feeda8c246546895
Zach10a <zachtait84@outlook.com>
2022-06-28 22:26:57 +0100

    Attempt basic data transfer from output to terminal

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 print("Builtin modules imported")
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, Slot
 
 print("QTCore imported")
 from PySide2.QtGui import QPixmap
@@ -12,6 +12,9 @@ print("QtGUI imported")
 from PySide2.QtWidgets import QMainWindow, QApplication, QSplashScreen
 
 from pterasoftware.ui_resources.mainWindow import Ui_MainWindowDesign
+from pterasoftware import aerodynamics, convergence, functions, geometry, meshing, movement, operating_point,output,\
+    panel, problems, steady_horseshoe_vortex_lattice_method, steady_ring_vortex_lattice_method, trim,\
+    unsteady_ring_vortex_lattice_method
 
 
 class MainWindow(QMainWindow, Ui_MainWindowDesign):
@@ -30,6 +33,8 @@ class MainWindow(QMainWindow, Ui_MainWindowDesign):
         self.actionExample_9.triggered.connect(lambda x: self.exampleMenu(8))
         self.actionExample_10.triggered.connect(lambda x: self.exampleMenu(9))
 
+        self.displayText = ""
+
     def exampleMenu(self, ex_num):
         import importlib
         files = []
@@ -38,11 +43,18 @@ class MainWindow(QMainWindow, Ui_MainWindowDesign):
             files.append(f)
         file = files[ex_num]
         file = file.replace(".py", "")
+        self.printTerminalOutput(f"Example {ex_num + 1} executed")
         print(file)
         importlib.import_module(file)
 
     def printTerminalOutput(self):
-        pass
+        print("Printing terminal output")
+        self.terminalOutput.addItem(f"{self.displayText}")
+
+    def updateDisplayText(self, text):
+        self.displayText = text
+        self.printTerminalOutput(self)
+
 
 
 if __name__ == '__main__':

c76098428132f45aedd8a4848de058a31f7105bc
Zach10a <zachtait84@outlook.com>
2022-08-06 16:49:11 +0100

    Change names of files

--- pterasoftware/ui_resources/main_window.py
+++ pterasoftware/ui_resources/main_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainWindow.ui'
+## Form generated from reading UI file 'main_window.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##

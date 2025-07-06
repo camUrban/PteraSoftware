852717a141e0ce1274d53af18fb39a0174fb5d38
camUrban <camerongurban@gmail.com>
2025-07-06 16:01:13 -0400

    I fixed some documentation typos and changed the array type of the output vertices to be floats.

--- pterasoftware/panel.py
+++ pterasoftware/panel.py
@@ -229,7 +229,7 @@ class Panel:
         """This method defines a property which is an estimate of the panel's area.
 
         This is only an estimate because the surface defined by four line segments in
-        3-space is a hyperboloid, and there doesn't seem to be a close-form equation
+        3-space is a hyperboloid, and there doesn't seem to be a closed-form equation
         for the surface area of a hyperboloid between four points. Instead,
         we estimate the area using the cross product of panel's diagonal vectors,
         which should be relatively accurate if the panel can be approximated as a

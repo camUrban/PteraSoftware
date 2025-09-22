121db6d7280ccc5a62fcacf0a98f37f3749c4fd0
Cameron Urban <camerongurban@gmail.com>
2025-09-21 22:39:53 -0400

    First stab at refactoring problems.py

--- pterasoftware/problems.py
+++ pterasoftware/problems.py
@@ -14,6 +14,7 @@ This module contains the following functions:
 import math
 
 
+# ToDo: Add unit tests for this class.
 class SteadyProblem:
     """This is a class for steady aerodynamics problems.
 
@@ -30,16 +31,19 @@ class SteadyProblem:
     def __init__(self, airplanes, operating_point):
         """This is the initialization method.
 
-        :param airplanes: list of Airplane objects
-            This is a list of the airplane objects for this problem.
+        :param airplanes: list of Airplanes
+
+            This is the list of the Airplanes for this SteadyProblem.
+
         :param operating_point: OperatingPoint
-            This is the operating point object for this problem.
+
+            This is the OperatingPoint for this SteadyProblem.
         """
-        # Initialize the problem's attributes.
         self.airplanes = airplanes
         self.operating_point = operating_point
 
 
+# ToDo: Add unit tests for this class.
 class UnsteadyProblem:
     """This is a class for unsteady aerodynamics problems.
 
@@ -57,28 +61,32 @@ class UnsteadyProblem:
         """This is the initialization method.
 
         :param movement: Movement
-            This is the movement object that contains this problem's airplane and
-            operating point objects.
+
+            This is the Movement that contains this UnsteadyProblem's
+            OperatingPointMovement and AirplaneMovements.
+
         :param only_final_results: Bool
-            If this is set true, the solver will only calculate forces, moments,
-            and pressures for the final complete cycle (of the movement with the
-            longest period), which increases speed. The default value is False.
+
+            If set to True, the Solver will only calculate forces, moments,
+            and pressures for the final complete cycle (of the Movement's
+            sub-Movement with the longest period), which increases simulation speed.
+            The default value is False.
         """
-        # Initialize the class attributes.
         self.movement = movement
         self.num_steps = self.movement.num_steps
         self.delta_time = self.movement.delta_time
         self.only_final_results = only_final_results
 
-        # Find the maximum period of this problem's movements.
+        # Find the maximum period of this UnsteadyProblem's Movement's sub-Movements.
         self.max_period = movement.get_max_period()
 
-        # For unsteady problems with static movement, users are typically interested
+        # For UnsteadyProblems with a static Movement, users are typically interested
         # in the final time step's forces and moments, which, assuming convergence,
-        # will be the most accurate. For unsteady problems with cyclic movement,
-        # users are typically interested in the forces and movements averaged over
-        # the last cycle simulated. Based on if the movement is static or cyclic,
-        # find the first time step with relevant results.
+        # will be the most accurate. For UnsteadyProblems with cyclic movement,
+        # (e.g. flapping wings) users are typically interested in the forces and
+        # moments averaged over the last cycle simulated. Therefore, determine which
+        # time step will be the first with relevant results based on if the Movement
+        # is static or cyclic.
         if self.max_period == 0:
             self.first_averaging_step = self.num_steps - 1
         else:
@@ -87,25 +95,25 @@ class UnsteadyProblem:
             )
 
         # If the user only wants to calculate forces and moments for the final cycle
-        # (for cyclic motion) or for the final time step (for static movement) set
-        # the first step to calculate results to the first averaging step. Otherwise,
-        # set it to the zero, which is the first time step.
+        # (for a cyclic Movement) or for the final time step (for a static Movement)
+        # set the first step to calculate results to the first averaging step.
+        # Otherwise, set it to the zero, which is the first time step.
         if self.only_final_results:
             self.first_results_step = self.first_averaging_step
         else:
             self.first_results_step = 0
 
         # Initialize empty lists to hold the final loads and load coefficients each
-        # airplane object experiences. These will only be populated for static
-        # geometry problems.
+        # Airplane experiences. These will only be populated if this
+        # UnsteadyProblem's Movement is static.
         self.final_near_field_forces_wind_axes = []
         self.final_near_field_force_coefficients_wind_axes = []
         self.final_near_field_moments_wind_axes = []
         self.final_near_field_moment_coefficients_wind_axes = []
 
         # Initialize empty lists to hold the final cycle-averaged loads and load
-        # coefficients each airplane object experiences. These will only be populated
-        # for variable geometry problems.
+        # coefficients each Airplane experiences. These will only be populated if
+        # this UnsteadyProblem's Movement is cyclic..
         self.final_mean_near_field_forces_wind_axes = []
         self.final_mean_near_field_force_coefficients_wind_axes = []
         self.final_mean_near_field_moments_wind_axes = []
@@ -119,25 +127,22 @@ class UnsteadyProblem:
         self.final_rms_near_field_moments_wind_axes = []
         self.final_rms_near_field_moment_coefficients_wind_axes = []
 
-        # Initialize an empty list to hold the steady problems.
+        # Initialize an empty list to hold the SteadyProblems.
         self.steady_problems = []
 
-        # Iterate through the problem's time steps.
+        # Iterate through the UnsteadyProblem's time steps.
         for step_id in range(self.num_steps):
 
-            # Get the airplane objects and the operating point object associated with
-            # this time step. This will be a list of the airplane snapshots associated
-            # with each base airplane at this particular time step.
+            # Get the Airplanes and the OperatingPoint associated with this time step.
             these_airplanes = []
             for this_base_airplane in movement.airplanes:
                 these_airplanes.append(this_base_airplane[step_id])
-
             this_operating_point = movement.operating_points[step_id]
 
-            # Initialize the steady problem object at this time step.
+            # Initialize the SteadyProblem at this time step.
             this_steady_problem = SteadyProblem(
                 airplanes=these_airplanes, operating_point=this_operating_point
             )
 
-            # Append this steady problem to the list of steady problems.
+            # Append this SteadyProblem to the list of SteadyProblems.
             self.steady_problems.append(this_steady_problem)

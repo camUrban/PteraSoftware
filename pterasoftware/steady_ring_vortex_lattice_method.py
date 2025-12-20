Fix Katz method sign error in pressure force calculation

pterasoftware/steady_ring_vortex_lattice_method.py
@@ -774,7 +774,7 @@ class SteadyRingVortexLatticeMethodSolver:
             # Calculate the force (in the first Airplane's geometry axes) on each
             # Panel from pressure integration.
             forces_GP1 = (
-                -np.expand_dims(stackPressureCoefficients, axis=1)
+                np.expand_dims(stackPressureCoefficients, axis=1)
                 * self.operating_point.qInf__E
                 * np.expand_dims(self.panel_areas, axis=1)
                 * self.stackUnitNormals_GP1